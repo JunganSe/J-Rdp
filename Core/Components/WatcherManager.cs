361222af4c33996f743264ddd7f555af305ac173
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2023-12-29 00:20:05 +0100

    WatcherManager to detect when config file changes

---
+++ Core/Components/WatcherManager.cs
@@ -0,0 +1,47 @@
+﻿using NLog;
+
+namespace Core.Components;
+
+public class WatcherManager
+{
+    private readonly Logger _logger = LogManager.GetCurrentClassLogger();
+    private readonly Controller _controller;
+
+    public WatcherManager(Controller controller)
+    {
+        _controller = controller;
+    }
+
+
+
+    public FileSystemWatcher GetConfigWatcher()
+    {
+        var watcher = new FileSystemWatcher();
+        watcher.Path = AppDomain.CurrentDomain.BaseDirectory;
+        watcher.Filter = "config.json";
+        watcher.EnableRaisingEvents = true;
+        watcher.NotifyFilter = NotifyFilters.FileName | NotifyFilters.CreationTime;
+        watcher.Created += OnConfigChanged;
+        watcher.Changed += OnConfigChanged;
+        watcher.Deleted += OnConfigChanged;
+        watcher.Error += OnError;
+        return watcher;
+    }
+
+
+
+    private void OnConfigChanged(object sender, FileSystemEventArgs e)
+    {
+        string type = e.ChangeType.ToString().ToLower();
+        _logger.Info($"Config file {type}: {e.FullPath}");
+        _controller.OnConfigChanged();
+    }
+
+    private void OnError(object sender, ErrorEventArgs e)
+    {
+        var exception = e.GetException();
+        if (exception == null)
+            return;
+        _logger.Warn(exception);
+    }
+}
