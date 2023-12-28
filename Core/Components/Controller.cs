0c134712d2fd709a203fefc940858e7d9a0f7e41
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2023-12-29 00:27:34 +0100

    Handling of config and watcher in controller

--- Core/Components/Controller.cs
+++ Core/Components/Controller.cs
@@ -1,20 +1,36 @@
 ﻿using Core.Interfaces;
+using Core.Models;
 
 namespace Core.Components;
 
 public class Controller
 {
     private readonly IUi _ui;
+    private readonly ConfigManager _configManager;
+    private readonly WatcherManager _watcherManager;
+    private List<Config> _configs = [];
 
     public Controller(IUi ui)
     {
         _ui = ui;
+        _configManager = ConfigManager.Instance;
+        _watcherManager = new WatcherManager(this);
     }
 
 
 
     public void Start()
     {
-        throw new NotImplementedException();
+        _configs = _configManager.GetConfigs();
+        var configWatcher = _watcherManager.GetConfigWatcher();
+
+        while (true)
+            Thread.Sleep(1000);
+    }
+
+    public void OnConfigChanged()
+    {
+        // TODO: Debounce: Avbryt om samma fil har ändrats inom 500ms.
+        _configs = _configManager.GetConfigs();
     }
 }
