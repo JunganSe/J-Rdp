58857873bcd4ffb36f57b901a04b4d91be57ba7b
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2023-12-29 11:24:24 +0100

    Debounces OnConfigChanged() if it was recently run

--- Core/Components/Controller.cs
+++ Core/Components/Controller.cs
@@ -5,10 +5,13 @@ namespace Core.Components;
 
 public class Controller
 {
+    private const int DEBOUNCE_MS = 500;
+
     private readonly IUi _ui;
     private readonly ConfigManager _configManager;
     private readonly WatcherManager _watcherManager;
     private List<Config> _configs = [];
+    private DateTime _lastConfigChange;
 
     public Controller(IUi ui)
     {
@@ -22,6 +25,7 @@ public class Controller
     public void Start()
     {
         _configs = _configManager.GetConfigs();
+        _lastConfigChange = DateTime.Now;
         var configWatcher = _watcherManager.GetConfigWatcher(OnConfigChanged);
 
         while (true)
@@ -30,7 +34,19 @@ public class Controller
 
     public void OnConfigChanged()
     {
-        // TODO: Debounce: Avbryt om samma fil har ändrats inom 500ms.
+        if (DebounceConfig())
+            return;
+
         _configs = _configManager.GetConfigs();
+        _lastConfigChange = DateTime.Now;
+    }
+
+
+
+    private bool DebounceConfig() // True if the last config change was within {DEBOUNCE_MS} milliseconds. 
+    {
+        var time = DateTime.Now - _lastConfigChange;
+        Console.WriteLine($"{DateTime.Now} - {time}");
+        return (time.TotalMilliseconds < DEBOUNCE_MS);
     }
 }
