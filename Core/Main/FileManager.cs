5517fe2b4a2445efa1ecbd86f6e1a6bedba7402f
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2024-01-10 20:46:48 +0100

    Methods for finding directories in path

--- Core/Main/FileManager.cs
+++ Core/Main/FileManager.cs
@@ -10,4 +10,47 @@ internal class FileManager
         return (!fileName.IsEmpty
             && FileSystemName.MatchesSimpleExpression(filter, fileName, ignoreCase: true));
     }
+
+    public static string GetLastExistingFolderName(string path)
+    {
+        var dir = new DirectoryInfo(path);
+        while (!dir.Exists)
+            dir = dir.Parent ?? dir.Root;
+        return dir.ToString();
+    }
+
+    /// <summary>
+    /// Returns the name of the first non-existing directory in the path.
+    /// Will throw ArgumentException if root drive does not exist, or if the full path already exists.
+    /// </summary>
+    public static string GetFirstMissingFolderName(string path)
+    {
+        var dir = new DirectoryInfo(path);
+        var firstMissingDirectory = GetFirstMissingDirectory(dir);
+        return firstMissingDirectory.Name;
+    }
+
+    /// <summary>
+    /// Returns the first non-existing directory in the path of targetDirectory.
+    /// Will throw ArgumentException if root drive does not exist, or if targetDirectory already exists.
+    /// </summary>
+    public static DirectoryInfo GetFirstMissingDirectory(DirectoryInfo targetDirectory)
+    {
+        if (!targetDirectory.Root.Exists)
+            throw new ArgumentException($"Drive '{targetDirectory.Root}' does not exist.");
+        if (targetDirectory.Exists)
+            throw new ArgumentException($"Directory already exists: {targetDirectory.FullName}");
+
+        var currentDir = targetDirectory;
+        while (!currentDir.Exists)
+        {
+            var parent = currentDir.Parent ?? currentDir.Root;
+            if (parent.Exists)
+                return currentDir;
+            currentDir = parent;
+        }
+
+        // Program should never reach this part, but it's needed to prevent compiler error CS0161: "not all code paths returns a value".
+        throw new Exception($"Unexpected state in {System.Reflection.MethodBase.GetCurrentMethod()?.Name}");
+    }
 }
