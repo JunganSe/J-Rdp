57e26ab6af7114be6a5e2330ed35a6cf63afc8b9
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2024-01-13 03:03:33 +0100

    Split FolderWatcher into 2 classes

---
+++ Core/Components/FolderRemovedWatcher.cs
@@ -0,0 +1,44 @@
+﻿using NLog;
+
+namespace Core.Components;
+
+internal class FolderRemovedWatcher : FileSystemWatcher
+{
+    private readonly Logger _logger = LogManager.GetCurrentClassLogger();
+    private readonly string _fullPath;
+    private readonly FileWatcher _fileWatcher;
+
+    public FolderRemovedWatcher(string fullPath, FileWatcher fileWatcher)
+    {
+        _fullPath = fullPath;
+        _fileWatcher = fileWatcher;
+
+        var dir = new DirectoryInfo(fullPath);
+
+        Path = dir.Parent?.FullName ?? dir.Root.FullName;
+        Filter = dir.Name;
+        EnableRaisingEvents = true;
+        IncludeSubdirectories = false;
+        NotifyFilter = NotifyFilters.DirectoryName;
+        Renamed += OnRenamed;
+        Deleted += OnDeleted;
+        Error += OnError;
+    }
+
+    private void OnRenamed(object sender, RenamedEventArgs args)
+    {
+        
+    }
+
+    private void OnDeleted(object sender, FileSystemEventArgs args)
+    {
+        
+    }
+
+    private void OnError(object sender, ErrorEventArgs args)
+    {
+        var exception = args.GetException();
+        if (exception != null)
+            _logger.Error(exception);
+    }
+}
--- Core/Components/FolderWatcher.cs
+++ Core/Components/FolderWatcher.cs
@@ -7,21 +7,13 @@ internal class FolderWatcher : FileSystemWatcher
 {
     private readonly Logger _logger = LogManager.GetCurrentClassLogger();
     private readonly string _fullPath;
-    private readonly string? _fileNameFilter;
-    private readonly FileWatcher? _governingFileWatcher;
+    private readonly string _fileNameFilter;
     private string _currentPath = "";
 
     public FolderWatcher(string fullPath, string fileNameFilter)
     {
         _fullPath = fullPath;
         _fileNameFilter = fileNameFilter;
-        Initialize();
-    }
-
-    public FolderWatcher(string fullPath, FileWatcher fileWatcher)
-    {
-        _fullPath = fullPath;
-        _governingFileWatcher = fileWatcher;
         Initialize();
     }
 
@@ -34,13 +26,10 @@ internal class FolderWatcher : FileSystemWatcher
         NotifyFilter = NotifyFilters.DirectoryName;
         Created += OnDetected;
         Renamed += OnRenamed;
-        Deleted += OnDeleted;
         Error += OnError;
         _currentPath = System.IO.Path.Combine(Path, Filter);
 
-        string fullPath = (_governingFileWatcher != null) 
-            ? FileSystemHelper.CombineAndNormalizePaths(_governingFileWatcher.Path, _governingFileWatcher.Filter) 
-            : FileSystemHelper.CombineAndNormalizePaths(_fullPath, _fileNameFilter ?? "");
+        string fullPath = FileSystemHelper.CombineAndNormalizePaths(_fullPath, _fileNameFilter);
         _logger.Info($"Watching for '{_currentPath}' in path '{fullPath}'.");
     }
 
@@ -64,15 +53,7 @@ internal class FolderWatcher : FileSystemWatcher
         if (found == target)
             OnDetected(this, args);
         else
-        {
-            // TODO: Hantera att mappen inte finns längre.
-        }
-    }
-
-    private void OnDeleted(object sender, FileSystemEventArgs args)
-    {
-        // TODO: Hantera att mappen inte finns längre.
-        throw new NotImplementedException();
+            Initialize();
     }
 
     private void OnError(object sender, ErrorEventArgs args)
