eaf6ec3011dfd710fdb3915fdb9103969054d453
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2024-02-06 19:46:48 +0100

    Created PollingController with todo comments

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,6 +6,6 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        new Controller().Start();
+        new PollingController().Start();
     }
 }
---
+++ Core/Main/PollingController.cs
@@ -0,0 +1,38 @@
+﻿using NLog;
+
+namespace Core.Main;
+
+public class PollingController
+{
+    private readonly Logger _logger = LogManager.GetCurrentClassLogger();
+    private readonly int _pollingInterval = 1000;
+
+    public PollingController()
+    {
+    }
+
+    public void Start()
+    {
+        while (true)
+        {
+            MainLoop();
+            Thread.Sleep(_pollingInterval);
+        }
+    }
+
+    private void MainLoop()
+    {
+        // TODO: Implement all this:
+        // - Read config file.
+        // - Check if config file has changed (compare with previous version)
+        //   - Update configs if needed.
+        // - Loop through configs.
+        //   - Check if the folder to watch exists.
+        //   - If folder exists, list all files in it.
+        //   - Ignore files that are already handled. (How?)
+        //   - Loop though files and check for filter match.
+        //   - If file matches filter:
+        //     - Register the file as handled. (How?)
+        //     - Run instructions on the file according to config.
+    }
+}
