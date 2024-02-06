bb47f0018f293b3648a4168fcd4795fd7a657c18
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2024-02-06 19:42:16 +0100

    Created Config & ConfigManager

---
+++ Core/Configuration/ConfigManager.cs
@@ -0,0 +1,81 @@
+﻿using NLog;
+using System.Text.Json;
+
+namespace Core.Configuration;
+
+internal class ConfigManager
+{
+    private const string CONFIG_FILE_NAME = "config.json";
+
+    private readonly Logger _logger = LogManager.GetCurrentClassLogger();
+    private readonly JsonSerializerOptions _jsonOptions;
+
+    public List<Config> Configs { get; private set; } = [];
+
+    public ConfigManager()
+    {
+        _jsonOptions = new JsonSerializerOptions() { PropertyNameCaseInsensitive = true };
+    }
+
+
+
+    public void UpdateConfigs()
+    {
+        Configs = GetConfigsFromFile();
+    }
+
+    public List<Config> GetConfigsFromFile()
+    {
+        try
+        {
+            string path = GetConfigPath();
+            string json = ReadFile(path);
+            return Parse(json);
+        }
+        catch
+        {
+            return [];
+        }
+    }
+
+
+
+    private string GetConfigPath()
+    {
+        string directory = AppDomain.CurrentDomain.BaseDirectory;
+        return Path.Combine(directory, CONFIG_FILE_NAME);
+    }
+
+    private string ReadFile(string path)
+    {
+        try
+        {
+            if (!File.Exists(path))
+                throw new ArgumentException("File does not exist.");
+
+            string json = File.ReadAllText(path);
+            _logger.Debug($"Successfully read file: {path}");
+            return json;
+        }
+        catch (Exception ex)
+        {
+            _logger.Error(ex, $"Failed to read file: {path}");
+            throw;
+        }
+    }
+
+    private List<Config> Parse(string json)
+    {
+        try
+        {
+            var configs = JsonSerializer.Deserialize<List<Config>>(json, _jsonOptions);
+            _logger.Debug("Successfully parsed configs from json.");
+            return configs ?? new List<Config>();
+        }
+        catch (Exception ex)
+        {
+            _logger.Error(ex, $"Failed to parse configs from json: {json}");
+            throw;
+        }
+    }
+}
