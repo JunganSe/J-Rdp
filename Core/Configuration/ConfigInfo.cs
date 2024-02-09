351898e8ec79f2f45def2f7cc3c295717dd18581
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2024-02-09 23:51:10 +0100

    Created ConfigInfo class

---
+++ Core/Configuration/ConfigInfo.cs
@@ -0,0 +1,23 @@
+﻿namespace Core.Configuration;
+
+internal class ConfigInfo
+{
+    public Config Config { get; }
+    public DirectoryInfo Directory { get; }
+    public IEnumerable<FileInfo> Files { get; private set; }
+
+    public ConfigInfo(Config config)
+    {
+        Config = config;
+        Directory = new DirectoryInfo(config.WatchFolder);
+        Files = [];
+    }
+
+    public void UpdateFiles()
+    {
+        if (Directory.Exists)
+            Files = Directory.GetFiles();
+        else
+            Files = [];
+    }
+}
