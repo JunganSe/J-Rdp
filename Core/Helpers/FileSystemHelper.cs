c4a3769dad3b00a5fc2577e86f342f360da1e58b
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2024-02-10 00:25:12 +0100

    Created FileSystemHelper with filter matching method

---
+++ Core/Helpers/FileSystemHelper.cs
@@ -0,0 +1,13 @@
+﻿using System.IO.Enumeration;
+
+namespace Core.Helpers;
+
+internal class FileSystemHelper
+{
+    public bool FileNameMatchesFilter(FileInfo file, string filter) 
+        => FileNameMatchesFilter(file.Name, filter);
+
+    public bool FileNameMatchesFilter(string fileName, string filter) 
+        => (!string.IsNullOrEmpty(fileName)
+            && FileSystemName.MatchesSimpleExpression(filter, fileName, ignoreCase: true));
+}
