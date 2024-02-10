031382c8bf908dcd8d4ad1b69f421a1938d2234f
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2024-02-10 02:04:57 +0100

    Cleanup

--- Core/Configuration/ConfigInfo.cs
+++ Core/Configuration/ConfigInfo.cs
@@ -7,7 +7,6 @@ internal class ConfigInfo
     private readonly FileInfoFullNameEqualityComparer _fileComparer = new FileInfoFullNameEqualityComparer();
 
     public Config Config { get; }
-    //public DirectoryInfo Directory { get; }
     public IEnumerable<FileInfo> Files { get; private set; }
     public IEnumerable<FileInfo> LastFiles { get; private set; }
     public IEnumerable<FileInfo> NewFiles => Files.Except(LastFiles, _fileComparer);
@@ -16,7 +15,6 @@ internal class ConfigInfo
     public ConfigInfo(Config config)
     {
         Config = config;
-        //Directory = new DirectoryInfo(config.WatchFolder);
         Files = [];
         LastFiles = [];
     }
