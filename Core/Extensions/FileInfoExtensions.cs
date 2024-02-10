dd2d869d32f5ce38001157b05396a9596b2d2409
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2024-02-10 01:43:23 +0100

    Replaced FileSystemHelper with extension method

---
+++ Core/Extensions/FileInfoExtensions.cs
@@ -0,0 +1,9 @@
+﻿using System.IO.Enumeration;
+
+namespace Core.Extensions;
+
+public static class FileInfoExtensions
+{
+    public static bool NameMatchesFilter(this FileInfo file, string filter, bool ignoreCase = true) =>
+        FileSystemName.MatchesSimpleExpression(filter, file.Name, ignoreCase);
+}
