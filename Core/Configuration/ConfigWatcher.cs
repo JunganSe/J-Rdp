4c6c7cabbb85a2f7adf70ef686e7f508960be7b2
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2024-03-19 22:09:44 +0100

    Added ConfigWatcher and FileSystemHelper

---
+++ Core/Configuration/ConfigWatcher.cs
@@ -0,0 +1,66 @@
+﻿using Core.Helpers;
+using NLog;
+
+namespace Core.Configuration;
+
+internal class ConfigWatcher : FileSystemWatcher
+{
+    private readonly Logger _logger = LogManager.GetCurrentClassLogger();
+
+    public Action? Callback { get; set; }
+
+    public ConfigWatcher(string path, string filter, Action callback)
+    {
+        Path = path;
+        Filter = filter;
+        Callback = callback;
+        EnableRaisingEvents = true;
+        IncludeSubdirectories = false;
+        NotifyFilter = NotifyFilters.FileName | NotifyFilters.LastWrite;
+        Created += OnChanged;
+        Changed += OnChanged;
+        Renamed += OnRenamed;
+        Deleted += OnMissing;
+        Error += OnError;
+    }
+
+
+
+    private void OnChanged(object sender, FileSystemEventArgs args)
+    {
+        if (sender != this)
+            return;
+
+        string eventType = args.ChangeType.ToString().ToLower();
+        _logger.Info($"Config file {eventType}: {args.FullPath}");
+
+        Callback?.Invoke();
+    }
+
+    private void OnRenamed(object sender, FileSystemEventArgs args)
+    {
+        if (sender != this)
+            return;
+
+        if (FileSystemHelper.FileNameMatchesFilter(args.FullPath, Filter))
+            OnChanged(this, args);
+        else
+            OnMissing(this, args);
+    }
+
+    private void OnMissing(object sender, FileSystemEventArgs args)
+    {
+        if (sender != this)
+            return;
+
+        _logger.Warn($"Config file '{Filter}' not found in {Path}");
+        Callback?.Invoke();
+    }
+
+    private void OnError(object sender, ErrorEventArgs args)
+    {
+        var exception = args.GetException();
+        if (exception != null)
+            _logger.Error(exception);
+    }
+}
---
+++ Core/Helpers/FileSystemHelper.cs
@@ -0,0 +1,13 @@
+﻿using System.IO.Enumeration;
+
+namespace Core.Helpers;
+
+internal static class FileSystemHelper
+{
+    public static bool FileNameMatchesFilter(string path, string filter)
+    {
+        ReadOnlySpan<char> fileName = Path.GetFileName(path);
+        return (!fileName.IsEmpty
+            && FileSystemName.MatchesSimpleExpression(filter, fileName, ignoreCase: true));
+    }
+}
