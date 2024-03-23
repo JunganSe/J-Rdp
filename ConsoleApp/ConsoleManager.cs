13a525ebcf6c5087e0414b7ebf47f8058cd8894b
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2024-03-23 09:44:08 +0100

    ConsoleManager: suppressed warnings, added link comment, reordered methods

--- ConsoleApp/ConsoleManager.cs
+++ ConsoleApp/ConsoleManager.cs
@@ -1,8 +1,13 @@
-﻿using NLog;
+﻿#pragma warning disable IDE1006 // Naming Styles
+#pragma warning disable SYSLIB1054 // Use 'LibraryImportAttribute' instead of 'DllImportAttribute' to generate P/Invoke marshalling code at compile time
+
+using NLog;
 using System.Runtime.InteropServices;
 
 static class ConsoleManager
 {
+    // https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-showwindow
+
     private const int SW_HIDE = 0;
     private const int SW_SHOW = 5;
     private static readonly IntPtr _handle = GetConsoleWindow();
@@ -11,15 +16,15 @@ static class ConsoleManager
     [DllImport("kernel32.dll")] static extern IntPtr GetConsoleWindow();
     [DllImport("user32.dll")] static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
 
-    public static void Hide()
-    {
-        _logger.Info("Hiding Console.");
-        ShowWindow(_handle, SW_HIDE); 
-    }
-
     public static void Show()
     {
         _logger.Info("Showing Console.");
         ShowWindow(_handle, SW_SHOW);
+    }
+
+    public static void Hide()
+    {
+        _logger.Info("Hiding Console.");
+        ShowWindow(_handle, SW_HIDE); 
     }
 }
