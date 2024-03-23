1c18ae08a6f59a635ddaa4b112d36eb323f972b7
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2024-03-23 01:03:01 +0100

    Added HideConsole argument and refactored parsing code

--- ConsoleApp/Arguments.cs
+++ ConsoleApp/Arguments.cs
@@ -3,6 +3,7 @@
 internal class Arguments
 {
     public int PollingInterval { get; set; }
+    public bool HideConsole { get; set; }
     public int LogLevel { get; set; } // TODO: Implement. 0: No logging, 1: Messages, 2: Messages + stack trace.
 
     public static Arguments Parse(string[] args)
@@ -14,9 +15,11 @@ internal class Arguments
             if (parts.Length != 2)
                 continue;
 
-            if (parts[0] == "PollingInterval")
+            if (parts[0] == nameof(PollingInterval))
                 output.PollingInterval = ParseInt(parts[1]);
-            else if (parts[0] == "LogLevel")
+            else if (parts[0] == nameof(HideConsole))
+                output.HideConsole = ParseBool(parts[1]);
+            else if (parts[0] == nameof(LogLevel))
                 output.LogLevel = ParseInt(parts[1]);
         }
         return output;
@@ -24,4 +27,7 @@ internal class Arguments
 
     private static int ParseInt(string input) 
         => (int.TryParse(input, out int result)) ? result : 0;
+
+    private static bool ParseBool(string input) 
+        => (bool.TryParse(input, out bool result)) ? result : false;
 }
