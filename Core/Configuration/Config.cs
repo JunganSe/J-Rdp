8967ab5a97bf71e4023e61aee02317054094eaa2
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2024-03-24 12:05:10 +0100

    Simplified Config model

--- Core/Configuration/Config.cs
+++ Core/Configuration/Config.cs
@@ -2,22 +2,11 @@
 
 internal class Config
 {
-    public string Name { get; set; }
-    public string WatchFolder { get; init; }
-    public string Filter { get; init; }
-    public string MoveToFolder { get; init; }
-    public bool Launch { get; set; }
-    public bool Delete { get; init; }
-    public List<string> Settings { get; init; }
-
-    public Config()
-    {
-        Name = "";
-        WatchFolder = "";
-        Filter = "";
-        MoveToFolder = "";
-        Launch = false;
-        Delete = false;
-        Settings = [];
-    }
+    public string Name { get; init; }           = "";
+    public string WatchFolder { get; init; }    = "";
+    public string Filter { get; init; }         = "";
+    public string MoveToFolder { get; init; }   = "";
+    public bool Launch { get; init; }           = false;
+    public bool Delete { get; init; }           = false;
+    public List<string> Settings { get; init; } = [];
 }
