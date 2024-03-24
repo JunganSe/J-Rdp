34645f7d77fafdc2a954eab56cbd6c505ccac5fb
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2024-03-24 16:15:54 +0100

    Adjusted max polling interval

--- Core/Constants/PollingInterval.cs
+++ Core/Constants/PollingInterval.cs
@@ -3,6 +3,6 @@
 internal static class PollingInterval
 {
     public const int Min = 100;
-    public const int Max = 10_000;
+    public const int Max = 30_000;
     public const int Default = 1000;
 }
