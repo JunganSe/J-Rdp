5fbe2db83365ec444689c8d1ac1a8e01cca333f9
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2024-04-13 09:00:49 +0200

    Set default values in config

--- Core/Configuration/Config.cs
+++ Core/Configuration/Config.cs
@@ -1,8 +1,10 @@
-﻿namespace Core.Configuration;
+﻿using Core.Constants;
+
+namespace Core.Configuration;
 
 internal class Config
 {
-    public int PollingInterval { get; set; }
-    public int DeleteDelay { get; set; }
+    public int PollingInterval { get; set; } = ConfigConstants.PollingInterval_Default;
+    public int DeleteDelay { get; set; } = ConfigConstants.DeleteDelay_Default;
     public IEnumerable<Profile> Profiles { get; set; } = [];
 }
