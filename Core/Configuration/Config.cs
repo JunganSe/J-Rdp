373c873efdd5599859ab98c9786f657266552793
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2024-04-13 08:26:37 +0200

    Create new config class

---
+++ Core/Configuration/Config.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.Configuration;
+
+internal class Config
+{
+    public int PollingInterval { get; set; }
+    public int DeleteDelay { get; set; }
+    public IEnumerable<Profile> Profiles { get; set; } = [];
+}
