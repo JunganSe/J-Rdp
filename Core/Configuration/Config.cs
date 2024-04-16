ee3a19a7da24f8cecd343611ae2556cbde5cfa83
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2024-04-16 20:56:03 +0200

    Adjusted whitespace

--- Core/Configuration/Config.cs
+++ Core/Configuration/Config.cs
@@ -4,7 +4,7 @@ namespace Core.Configuration;
 
 internal class Config
 {
-    public int PollingInterval { get; set; } = ConfigConstants.PollingInterval_Default;
-    public int DeleteDelay { get; set; } = ConfigConstants.DeleteDelay_Default;
+    public int PollingInterval { get; set; }    = ConfigConstants.PollingInterval_Default;
+    public int DeleteDelay { get; set; }        = ConfigConstants.DeleteDelay_Default;
     public List<Profile> Profiles { get; set; } = [];
 }
