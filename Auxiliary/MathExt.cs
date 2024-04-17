f3e0c66c9c19305b9266282ed83402b3025ffcca
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2024-04-17 21:28:32 +0200

    MathExt class to get median

---
+++ Auxiliary/MathExt.cs
@@ -0,0 +1,11 @@
+﻿namespace Auxiliary;
+
+public partial class MathExt
+{
+    public static T Median<T>(T a, T b, T c) where T : struct, IComparable<T>
+    {
+        T[] values = [a, b, c];
+        Array.Sort(values);
+        return values[1];
+    }
+}
