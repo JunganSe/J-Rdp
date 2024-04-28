c6d654ccc4d20de26acf53d00f900f05dd5365e2
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2024-04-28 21:44:21 +0200

    Implemented enable/disable logging to file

--- Auxiliary/LogManager.cs
+++ Auxiliary/LogManager.cs
@@ -1,12 +1,45 @@
 ﻿using NLog;
+using NLog.Config;
+using NLog.Targets;
 
 namespace Auxiliary;
 
 public static class LogManager
 {
+    private const string _fileRuleName = "file";
+
     public static void Initialize()
     {
         var assembly = typeof(LogManager).Assembly;
         NLog.LogManager.Setup().LoadConfigurationFromAssemblyResource(assembly);
+    }
+
+    public static void SetFileLogging(bool enabled)
+    {
+        if (enabled)
+            EnableFileLogging();
+        else
+            DisableFileLogging();
+    }
+
+    public static void EnableFileLogging()
+    {
+        var fileRule = GetLoggingRule(_fileRuleName);
+        fileRule?.EnableLoggingForLevels(LogLevel.Trace, LogLevel.Fatal);
+        NLog.LogManager.ReconfigExistingLoggers();
+    }
+
+    public static void DisableFileLogging()
+    {
+        var fileRule = GetLoggingRule(_fileRuleName);
+        fileRule?.DisableLoggingForLevels(LogLevel.Trace, LogLevel.Fatal);
+        NLog.LogManager.ReconfigExistingLoggers();
+    }
+
+    private static LoggingRule? GetLoggingRule(string ruleName)
+    {
+        var config = NLog.LogManager.Configuration;
+        var fileTarget = config.FindTargetByName(ruleName) as FileTarget;
+        return config.LoggingRules.FirstOrDefault(r => r.Targets.Contains(fileTarget));
     }
 }
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -10,11 +10,14 @@ internal class Program
         Console.Title = "J-Rdp 0.1.0";
 
         LogManager.Initialize();
+        LogManager.DisableFileLogging();
+
         var logger = NLog.LogManager.GetCurrentClassLogger();
         logger.Info("Starting application...");
 
         var arguments = Arguments.Parse(args);
         ConsoleManager.SetVisibility(!arguments.HideConsole);
+        LogManager.SetFileLogging(arguments.LogToFile);
 
         new Controller().Run();
 
