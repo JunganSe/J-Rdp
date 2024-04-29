1050fceb0956f5b19a9f112fd98abc5e95a4fed1
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2024-04-29 21:45:26 +0200

    Retry file read n times if IOException is encountered

---
+++ Core/Constants/FileConstants.cs
@@ -0,0 +1,7 @@
+﻿namespace Core.Constants;
+
+internal static class FileConstants
+{
+    public const int ReadFile_TryCountMax = 3; // Must be >= 1
+    public const int ReadFile_RetryDelay = 200;
+}
