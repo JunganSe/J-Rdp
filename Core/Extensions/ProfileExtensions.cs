663e38fbffe37f36aec705acadcd22aea69a821d
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2024-05-05 18:26:28 +0200

    Cleanup

--- Core/Extensions/ProfileExtensions.cs
+++ Core/Extensions/ProfileExtensions.cs
@@ -8,8 +8,6 @@ internal static class ProfileExtensions
     public static void RemoveDisabledProfiles(this IEnumerable<Profile> profiles) 
         => profiles = profiles.Where(p => p.Enabled);
 
-    public static void RemoveInvalidProfiles(this IEnumerable<Profile> profiles)
-    {
-        profiles = profiles.Where(p => ProfileHelper.IsProfileValid(p, out _));
-    }
+    public static void RemoveInvalidProfiles(this IEnumerable<Profile> profiles) 
+        => profiles = profiles.Where(p => ProfileHelper.IsProfileValid(p, out _));
 }
