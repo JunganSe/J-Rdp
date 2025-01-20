291fa3a5a5cf8be22b50787d988828778874b76a
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-01-20 22:22:33 +0100

    Cleanup whitespace

--- Auxiliary/AssemblyHelper.cs
+++ Auxiliary/AssemblyHelper.cs
@@ -4,16 +4,16 @@ namespace Auxiliary;
 
 public static class AssemblyHelper
 {
-    public static string GetAssemblyName(Type type)
-        => GetAssemblyName(type.Assembly);
+    public static string GetAssemblyName(Type type) =>
+        GetAssemblyName(type.Assembly);
 
-    public static string GetAssemblyName(Assembly assembly) 
-        => assembly.GetName().Name ?? "";
+    public static string GetAssemblyName(Assembly assembly) =>
+        assembly.GetName().Name ?? "";
 
 
 
-    public static string GetAssemblyVersion(Type type)
-        => GetAssemblyVersion(type.Assembly);
+    public static string GetAssemblyVersion(Type type) =>
+        GetAssemblyVersion(type.Assembly);
 
     public static string GetAssemblyVersion(Assembly assembly)
     {
