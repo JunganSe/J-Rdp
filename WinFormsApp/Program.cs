93588674add4c79f3d2eb50fcb50a2e577f1c39a
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-01-23 21:54:13 +0100

    Init winforms app

---
+++ WinFormsApp/Program.cs
@@ -0,0 +1,13 @@
+namespace WinFormsApp;
+
+internal static class Program
+{
+    [STAThread]
+    static void Main()
+    {
+        // To customize application configuration such as set high DPI settings or default font,
+        // see https://aka.ms/applicationconfiguration.
+        ApplicationConfiguration.Initialize();
+        Application.Run(new MainForm());
+    }
+}
