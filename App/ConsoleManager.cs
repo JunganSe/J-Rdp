15c4e96e9406311e770ea2ec9c61de2fb94e264c
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-01-26 22:23:25 +0100

    Redirect output to console

--- App/ConsoleManager.cs
+++ App/ConsoleManager.cs
@@ -25,14 +25,28 @@ namespace App
 
         private static void OpenConsole()
         {
-            AllocConsole();
+            bool isSuccess = AllocConsole();
+            if (!isSuccess)
+            {
+                _logger.Warn("Failed to open console.");
+                return;
+            }
+
+            // Redirect standard output and error streams to the console
+            var consoleOut = new StreamWriter(Console.OpenStandardOutput()) { AutoFlush = true };
+            Console.SetOut(consoleOut);
+            Console.SetError(consoleOut);
+
             _logger.Info("Opened console.");
         }
 
         private static void CloseConsole()
         {
-            FreeConsole();
-            _logger.Info("Closed console.");
+            bool isSuccess = FreeConsole();
+            if (isSuccess)
+                _logger.Info("Closed console.");
+            else
+                _logger.Warn("Failed to close console.");
         }
     }
 }
