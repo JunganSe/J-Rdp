b346aa6fab6bde20027b91a52dd024a4e37bd936
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-01-27 19:57:14 +0100

    Close console on ctrl+C without killing the app

--- App/ConsoleManager.cs
+++ App/ConsoleManager.cs
@@ -38,6 +38,7 @@ namespace App
             }
 
             SetConsoleTitle(_consoleTitle);
+            RegisterCloseEvents();
 
             // Redirect standard output and error streams to the console
             var consoleOut = new StreamWriter(Console.OpenStandardOutput()) { AutoFlush = true };
@@ -45,11 +46,24 @@ namespace App
             Console.SetError(consoleOut);
 
             _logger.Info("Opened console.");
+
+            
+        }
+
+        private static void RegisterCloseEvents()
+        {
+            Console.CancelKeyPress += OnCancelKeyPress;
+        }
+
+        private static void OnCancelKeyPress(object? sender, ConsoleCancelEventArgs eventArgs)
+        {
+            eventArgs.Cancel = true; // Prevents the console from closing, and taking the main app with it.
+            CloseConsole();
         }
 
         private static void CloseConsole()
         {
-            bool isSuccess = FreeConsole();
+            bool isSuccess = FreeConsole(); // Close the console without closing the main app.
             if (isSuccess)
                 _logger.Info("Closed console.");
             else
