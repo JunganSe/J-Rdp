d44a94ab037f24615040c679457679f279e1de99
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-02-05 23:01:07 +0100

    Add dummy context menu

--- WinApp/TrayManager.cs
+++ WinApp/TrayManager.cs
@@ -1,6 +1,6 @@
 ﻿namespace WinApp;
 
-internal class TrayManager
+internal static class TrayManager
 {
     internal static NotifyIcon GetNotifyIcon()
     {
@@ -9,6 +9,34 @@ internal class TrayManager
             Text = "J-Rdp",
             Icon = SystemIcons.Application,
             Visible = true,
+            ContextMenuStrip = GetContextMenu(),
         };
+    }
+
+    private static ContextMenuStrip GetContextMenu()
+    {
+        var contextMenu = new ContextMenuStrip();
+
+        var item1 = new ToolStripMenuItem("Item 1", null, OnClick_Item1) { Tag = "Tag1" };
+        contextMenu.Items.Add(item1);
+
+        var exitMenuItem = new ToolStripMenuItem("Exit", null, OnClick_Exit);
+        contextMenu.Items.Add(exitMenuItem);
+
+        return contextMenu;
+    }
+
+    private static void OnClick_Item1(object sender, EventArgs e)
+    {
+        if (sender is ToolStripMenuItem menuItem)
+        {
+            var tagValue = menuItem.Tag?.ToString();
+            MessageBox.Show($"Clicked Item 1 with Tag: {tagValue}");
+        }
+    }
+
+    private static void OnClick_Exit(object? sender, EventArgs e)
+    {
+        Application.Exit();
     }
 }
