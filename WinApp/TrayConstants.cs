e11d7f644d8546f748138815fe50bc2695ae0077
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-02-06 22:15:19 +0100

    Add TrayConstants

---
+++ WinApp/TrayConstants.cs
@@ -0,0 +1,18 @@
+﻿namespace WinApp;
+
+internal class TrayConstants
+{
+    public class ItemNames
+    {
+        public const string LogToFile = "LogToFile";
+        public const string ShowConsole = "ShowConsole";
+        public const string Exit = "Exit";
+    }
+
+    public class ItemTexts
+    {
+        public const string LogToFile = "Log to file";
+        public const string ShowConsole = "Show log console";
+        public const string Exit = "Exit";
+    }
+}
