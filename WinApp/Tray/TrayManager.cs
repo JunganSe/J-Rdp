498556043745c947e3208cc72118922a515b1934
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-02-06 22:35:01 +0100

    Add menu items

--- WinApp/Tray/TrayManager.cs
+++ WinApp/Tray/TrayManager.cs
@@ -15,23 +15,49 @@ internal static class TrayManager
 
     private static ContextMenuStrip GetContextMenu()
     {
-        var contextMenu = new ContextMenuStrip();
+        var contextMenu = new ContextMenuStrip()
+        {
+            AutoClose = false,
+        };
+        
+        contextMenu.Items.Add(new ToolStripMenuItem(null, null, OnClick_ShowConsole)
+        {
+            Name = TrayConstants.ItemNames.ShowConsole,
+            Text = TrayConstants.ItemTexts.ShowConsole,
+            CheckOnClick = true,
+        });
+        
+        contextMenu.Items.Add(new ToolStripMenuItem(null, null, OnClick_LogToFile)
+        {
+            Name = TrayConstants.ItemNames.LogToFile,
+            Text = TrayConstants.ItemTexts.LogToFile,
+            CheckOnClick = true,
+        });
 
-        var item1 = new ToolStripMenuItem("Item 1", null, OnClick_Item1) { Tag = "Tag1" };
-        contextMenu.Items.Add(item1);
+        contextMenu.Items.Add(new ToolStripSeparator());
 
-        var exitMenuItem = new ToolStripMenuItem("Exit", null, OnClick_Exit);
-        contextMenu.Items.Add(exitMenuItem);
+        contextMenu.Items.Add(new ToolStripMenuItem(null, null, OnClick_Exit)
+        {
+            Name = TrayConstants.ItemNames.Exit,
+            Text = TrayConstants.ItemTexts.Exit,
+        });
 
         return contextMenu;
     }
 
-    private static void OnClick_Item1(object sender, EventArgs e)
+    private static void OnClick_ShowConsole(object sender, EventArgs e)
     {
         if (sender is ToolStripMenuItem menuItem)
         {
-            var tagValue = menuItem.Tag?.ToString();
-            MessageBox.Show($"Clicked Item 1 with Tag: {tagValue}");
+            // TODO: Toggle console visibility.
+        }
+    }
+
+    private static void OnClick_LogToFile(object sender, EventArgs e)
+    {
+        if (sender is ToolStripMenuItem menuItem)
+        {
+            // TODO: Toggle logging to file.
         }
     }
 
