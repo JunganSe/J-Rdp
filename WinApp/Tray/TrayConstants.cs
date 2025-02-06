6a8f9a00808b571c3f1bbbdfbef0e8184a2a0393
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-02-06 22:34:56 +0100

    Reorder constants

--- WinApp/Tray/TrayConstants.cs
+++ WinApp/Tray/TrayConstants.cs
@@ -4,15 +4,15 @@ internal static class TrayConstants
 {
     public static class ItemNames
     {
-        public const string LogToFile = "LogToFile";
         public const string ShowConsole = "ShowConsole";
+        public const string LogToFile = "LogToFile";
         public const string Exit = "Exit";
     }
 
     public static class ItemTexts
     {
-        public const string LogToFile = "Log to file";
         public const string ShowConsole = "Show log console";
+        public const string LogToFile = "Log to file";
         public const string Exit = "Exit";
     }
 }
