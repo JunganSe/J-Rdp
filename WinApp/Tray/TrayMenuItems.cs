a6bdd1edbe90017cbfe5b7902dec8dd2e5045ad8
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-02-08 00:03:21 +0100

    Simplify instantiation

--- WinApp/Tray/TrayMenuItems.cs
+++ WinApp/Tray/TrayMenuItems.cs
@@ -2,27 +2,27 @@
 
 internal static class TrayMenuItems
 {
-    public static ToolStripMenuItem ToggleConsole = new ToolStripMenuItem(null, null, TrayMenuEvents.OnClick_ToggleConsole)
+    public static ToolStripMenuItem ToggleConsole = new(null, null, TrayMenuEvents.OnClick_ToggleConsole)
     {
         Name = TrayConstants.ItemNames.ToggleConsole,
         Text = TrayConstants.ItemTexts.ToggleConsole,
         CheckOnClick = true,
     };
 
-    public static ToolStripMenuItem ToggleLogToFile = new ToolStripMenuItem(null, null, TrayMenuEvents.OnClick_ToggleLogToFile)
+    public static ToolStripMenuItem ToggleLogToFile = new(null, null, TrayMenuEvents.OnClick_ToggleLogToFile)
     {
         Name = TrayConstants.ItemNames.ToggleLogToFile,
         Text = TrayConstants.ItemTexts.ToggleLogToFile,
         CheckOnClick = true,
     };
 
-    public static ToolStripMenuItem Exit = new ToolStripMenuItem(null, null, TrayMenuEvents.OnClick_Exit)
+    public static ToolStripMenuItem Exit = new(null, null, TrayMenuEvents.OnClick_Exit)
     {
         Name = TrayConstants.ItemNames.Exit,
         Text = TrayConstants.ItemTexts.Exit,
     };
 
-    public static ToolStripMenuItem Cancel = new ToolStripMenuItem(null, null, TrayMenuEvents.OnClick_Cancel)
+    public static ToolStripMenuItem Cancel = new(null, null, TrayMenuEvents.OnClick_Cancel)
     {
         Name = TrayConstants.ItemNames.Cancel,
         Text = TrayConstants.ItemTexts.Cancel,
