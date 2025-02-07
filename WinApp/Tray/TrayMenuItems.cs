ecffebe593653d8fd71a51da0e854b1f481ba565
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-02-07 23:49:00 +0100

    Add tray menu items

---
+++ WinApp/Tray/TrayMenuItems.cs
@@ -0,0 +1,30 @@
+﻿namespace WinApp.Tray;
+
+internal static class TrayMenuItems
+{
+    public static ToolStripMenuItem ShowConsole = new ToolStripMenuItem(null, null, OnClick_ShowConsole)
+    {
+        Name = TrayConstants.ItemNames.ShowConsole,
+        Text = TrayConstants.ItemTexts.ShowConsole,
+        CheckOnClick = true,
+    };
+
+    public static ToolStripMenuItem LogToFile = new ToolStripMenuItem(null, null, OnClick_LogToFile)
+    {
+        Name = TrayConstants.ItemNames.LogToFile,
+        Text = TrayConstants.ItemTexts.LogToFile,
+        CheckOnClick = true,
+    };
+
+    public static ToolStripMenuItem Exit = new ToolStripMenuItem(null, null, OnClick_Exit)
+    {
+        Name = TrayConstants.ItemNames.Exit,
+        Text = TrayConstants.ItemTexts.Exit,
+    };
+
+    public static ToolStripMenuItem Cancel = new ToolStripMenuItem(null, null, OnClick_Close)
+    {
+        Name = TrayConstants.ItemNames.Cancel,
+        Text = TrayConstants.ItemTexts.Cancel,
+    };
+}
