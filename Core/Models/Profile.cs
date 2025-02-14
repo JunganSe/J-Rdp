2591358537b6a3750ea8c9430af7bc135a3ccb16
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-02-14 18:26:51 +0100

    Use One-time setter for Profile.Id

--- Core/Models/Profile.cs
+++ Core/Models/Profile.cs
@@ -4,7 +4,9 @@ namespace Core.Models;
 
 public class Profile
 {
-    public int Id { get; init; }                = -1;
+    private const int _defaultId = -1;
+
+    public int Id { get; private set; }         = _defaultId;
     public bool Enabled { get; set; }           = true;
     public string Name { get; init; }           = ConfigConstants.Profile_DefaultName;
     public string WatchFolder { get; init; }    = "";
@@ -13,4 +15,10 @@ public class Profile
     public bool Launch { get; init; }           = false;
     public bool Delete { get; init; }           = false;
     public List<string> Settings { get; init; } = [];
+
+    public void SetId(int id)
+    {
+        if (Id == _defaultId)
+            Id = id;
+    }
 }
