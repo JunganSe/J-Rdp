43a336a3a0661907434c8e4cbf1e508ce038f1cd
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-02-15 08:20:17 +0100

    Create FileWriter

---
+++ Core/Workers/FileWriter.cs
@@ -0,0 +1,16 @@
+﻿namespace Core.Workers;
+
+internal class FileWriter
+{
+    public void WriteFile(string path, string content)
+    {
+        throw new NotImplementedException();
+
+        // v1
+        using var writer = new StreamWriter(path, false);
+        writer.Write(content);
+
+        // v2
+        File.WriteAllText(path, content);
+    }
+}
