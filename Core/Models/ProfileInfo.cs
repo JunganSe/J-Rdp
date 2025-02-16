dcd57a46c67a59da00d27ecb61fee8bf95864280
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-02-16 22:04:45 +0100

    Add ProfileInfo class

---
+++ Core/Models/ProfileInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.Models;
+
+public class ProfileInfo
+{
+    public int Id { get; set; }       = -1;
+    public bool Enabled { get; set; } = false;
+    public string Name { get; set; }  = "";
+}
