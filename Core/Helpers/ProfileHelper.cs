f95885165ed836325abc1a3f853f95533f6398bd
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-02-17 22:12:09 +0100

    Create ProfileHelper with deep copy method

---
+++ Core/Helpers/ProfileHelper.cs
@@ -0,0 +1,19 @@
+﻿using Core.Models;
+
+namespace Core.Helpers;
+
+internal static class ProfileHelper
+{
+    /// <summary> Creates a deep copy of a profile. Id is not copied. </summary>
+    public static Profile DeepCopy(Profile profile) => new()
+    {
+        Enabled = profile.Enabled,
+        Name = profile.Name,
+        WatchFolder = profile.WatchFolder,
+        Filter = profile.Filter,
+        MoveToFolder = profile.MoveToFolder,
+        Launch = profile.Launch,
+        Delete = profile.Delete,
+        Settings = [.. profile.Settings]
+    };
+}
