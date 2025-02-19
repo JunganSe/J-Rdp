dc9449926cc22e339c31a09f8bc514ed25425b21
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-02-19 23:23:28 +0100

    Add unit tests for ProfileHelper

---
+++ Tests/Core.Tests/ProfileHelperTests.cs
@@ -0,0 +1,122 @@
+﻿using Core.Helpers;
+using Core.Models;
+
+namespace Core.Tests;
+
+[TestClass()]
+public class ProfileHelperTests
+{
+    [TestMethod()]
+    public void GetDeepCopies_Valid()
+    {
+        // Arrange
+        var expectedProfiles = GetMockProfiles();
+
+        // Act
+        var actualProfiles1 = ProfileHelper.GetDeepCopies(expectedProfiles);
+        var actualProfiles2 = ProfileHelper.GetDeepCopies(expectedProfiles, copyId: true);
+        var actualProfiles3 = ProfileHelper.GetDeepCopies(expectedProfiles, copyId: false);
+
+        // Assert
+        for (int i = 0; i < expectedProfiles.Count; i++)
+        {
+            AssertProfilesAreEqual(expectedProfiles[i], actualProfiles1[i], compareId: true);
+            AssertProfilesAreEqual(expectedProfiles[i], actualProfiles2[i], compareId: true);
+            AssertProfilesAreEqual(expectedProfiles[i], actualProfiles3[i], compareId: false);
+            Assert.AreNotEqual(expectedProfiles[i].Id, actualProfiles3[i].Id);
+        }
+    }
+
+    [TestMethod()]
+    public void GetDeepCopy_Valid()
+    {
+        // Arrange
+        var expected = GetMockProfiles().First();
+
+        // Act
+        var actual1 = ProfileHelper.GetDeepCopy(expected);
+        var actual2 = ProfileHelper.GetDeepCopy(expected, copyId: true);
+        var actual3 = ProfileHelper.GetDeepCopy(expected, copyId: false);
+
+        // Assert
+        AssertProfilesAreEqual(expected, actual1, compareId: true);
+        AssertProfilesAreEqual(expected, actual2, compareId: true);
+        AssertProfilesAreEqual(expected, actual3, compareId: false);
+        Assert.AreNotEqual(expected.Id, actual3.Id);
+    }
+
+    [TestMethod()]
+    public void SetEnabledStatesFromMatchingProfileInfos_Valid()
+    {
+        // Arrange
+        var profiles = GetMockProfiles();
+        var profileInfos = new List<ProfileInfo>
+        {
+            new() { Id = 1, Enabled = false },
+            new() { Id = 2, Enabled = true },
+            new() { Id = 3},
+        };
+
+        // Act
+        ProfileHelper.SetEnabledStatesFromMatchingProfileInfos(profiles, profileInfos);
+
+        // Assert
+        Assert.IsFalse(profiles[0].Enabled);
+        Assert.IsTrue(profiles[1].Enabled);
+        Assert.IsFalse(profiles[2].Enabled);
+    }
+
+    private void AssertProfilesAreEqual(Profile expected, Profile actual, bool compareId)
+    {
+        if (compareId)
+            Assert.AreEqual(expected.Id, actual.Id);
+
+        Assert.AreEqual(expected.Enabled, actual.Enabled);
+        Assert.AreEqual(expected.Name, actual.Name);
+        Assert.AreEqual(expected.WatchFolder, actual.WatchFolder);
+        Assert.AreEqual(expected.Filter, actual.Filter);
+        Assert.AreEqual(expected.MoveToFolder, actual.MoveToFolder);
+        Assert.AreEqual(expected.Launch, actual.Launch);
+        Assert.AreEqual(expected.Delete, actual.Delete);
+        CollectionAssert.AreEqual(expected.Settings, actual.Settings);
+    }
+
+    #region Mocks
+    private List<Profile> GetMockProfiles() =>
+    [
+        new(id: 1)
+        {
+            Enabled = true,
+            Name = "Profile 1",
+            WatchFolder = "C:\\WatchFolder1",
+            Filter = "Filter1",
+            MoveToFolder = "C:\\MoveToFolder1",
+            Launch = true,
+            Delete = true,
+            Settings = ["Setting1a", "Setting1b"],
+        },
+        new(id: 2)
+        {
+            Enabled = false,
+            Name = "Profile 2",
+            WatchFolder = "C:\\WatchFolder2",
+            Filter = "Filter2",
+            MoveToFolder = "C:\\MoveToFolder2",
+            Launch = false,
+            Delete = false,
+            Settings = ["Setting2a", "Setting2b"],
+        },
+        new(id: 3)
+        {
+            Enabled = false,
+            Name = "Profile 3",
+            WatchFolder = "C:\\WatchFolder3",
+            Filter = "Filter3",
+            MoveToFolder = "C:\\MoveToFolder3",
+            Launch = false,
+            Delete = false,
+            Settings = ["Setting3a", "Setting3b"],
+        },
+    ];
+    #endregion
+}
