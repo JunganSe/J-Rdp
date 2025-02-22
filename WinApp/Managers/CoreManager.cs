da48760758741fffc49649dedecea1a072cbc581
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-02-22 22:15:26 +0100

    Add (unfinished) CoreManager

---
+++ WinApp/Managers/CoreManager.cs
@@ -0,0 +1,24 @@
+﻿using Core.Models;
+
+namespace WinApp.Managers;
+
+internal class CoreManager
+{
+    private Core.Controller? _controller;
+
+    public void Initialize()
+    {
+        _controller = new();
+        //_controller.SetCallback_ConfigUpdated(dummy);
+    }
+
+    public void Run()
+    {
+        _controller?.Run();
+    }
+
+    public void UpdateProfilesEnabledState(List<ProfileInfo> profileInfos)
+    {
+        _controller?.UpdateProfilesEnabledState(profileInfos);
+    }
+}
