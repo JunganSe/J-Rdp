2e4d9553ddf096ff7c7be8a07299cdd135cf2389
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-02-22 22:15:40 +0100

    Add empty WinApp controller

---
+++ WinApp/Controller.cs
@@ -0,0 +1,10 @@
+﻿using WinApp.Managers;
+using WinApp.Tray;
+
+namespace WinApp;
+
+internal class Controller
+{
+    private readonly CoreManager _coreManager = new();
+    private readonly TrayManager _trayManager = new();
+}
