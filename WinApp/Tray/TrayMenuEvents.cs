58167bf2ce88f6f716c8d82c2040d2431f6a3594
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-03-04 21:53:28 +0100

    Whitespace

--- WinApp/Tray/TrayMenuEvents.cs
+++ WinApp/Tray/TrayMenuEvents.cs
@@ -8,17 +8,13 @@ internal static class TrayMenuEvents
     public static void OnClick_ToggleConsole(object? sender, EventArgs e)
     {
         if (sender is ToolStripMenuItem menuItem)
-        {
             ConsoleManager.SetVisibility(menuItem.Checked);
-        }
     }
 
     public static void OnClick_ToggleLogToFile(object? sender, EventArgs e)
     {
         if (sender is ToolStripMenuItem menuItem)
-        {
             LogManager.SetFileLogging(menuItem.Checked);
-        }
     }
 
     public static void OnClick_Exit(object? sender, EventArgs e)
