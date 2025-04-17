0896e8c1c604cb3539c4de488699536fd5739be6
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-04-17 23:40:22 +0200

    Update start bat file

--- WinApp/J-Rdp start.bat
+++ WinApp/J-Rdp start.bat
@@ -1,1 +1,5 @@
-start "J-Rdp" J-Rdp.exe -ShowConsole -LogToFile
+start "J-Rdp" J-Rdp.exe -LogToFile
+:: Available arguments (without quotes):
+:: "-LogToFile"    Activate logging to file at startup.
+:: "-ShowConsole"  Show the log console at startup.
+:: "-NoTray"       Run silently without the tray icon and menu.
