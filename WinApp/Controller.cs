b8549319b8f6847ecd0d0ede671caaffd695e5ae
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-04-29 21:42:01 +0200

    Comment

--- WinApp/Controller.cs
+++ WinApp/Controller.cs
@@ -13,7 +13,7 @@ internal class Controller
     public void Run(Arguments arguments)
     {
         Initialize(arguments); // Initialize on the current thread.
-        Task.Run(_coreManager.Run); // Run CoreManager on a new thread.
+        Task.Run(_coreManager.Run); // Run CoreManager asynchronously (as if on another thread).
     }
 
     private void Initialize(Arguments arguments)
