d5ecd7d9452393f9797486f475f047d9af0bb160
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-05-25 21:21:00 +0200

    Add StopSignalListener unit test

---
+++ Tests/WinAppTests/App/StopSignalListenerTests.cs
@@ -0,0 +1,31 @@
+﻿using System.IO.Pipes;
+using WinApp.App;
+
+namespace WinAppTests.App;
+
+[TestClass]
+public class StopSignalListenerTests
+{
+    [TestMethod]
+    public void Start_ConnectsAndInvokesCallback()
+    {
+        // Arrange
+        var listener = new StopSignalListener();
+        var callbackInvoked = new ManualResetEventSlim(false);
+        void Callback() => callbackInvoked.Set();
+
+        // Act
+        listener.Start(Callback);
+
+        using var clientTask = Task.Run(async () =>
+        {
+            await Task.Delay(100);
+            using var client = new NamedPipeClientStream(".", "J-Rdp.Stop", PipeDirection.Out);
+            await client.ConnectAsync(1000);
+        });
+
+        // Assert
+        bool signalWasSet = callbackInvoked.Wait(2000);
+        Assert.IsTrue(signalWasSet, "Callback was not invoked when stop signal was sent.");
+    }
+}
