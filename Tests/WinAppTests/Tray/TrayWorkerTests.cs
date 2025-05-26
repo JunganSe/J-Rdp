c07fa3abd73e6b0ea4e6dccced07740c7a5f7679
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-05-26 22:20:46 +0200

    Init TrayWorkerTests class

---
+++ Tests/WinAppTests/Tray/TrayWorkerTests.cs
@@ -0,0 +1,15 @@
+﻿using WinApp.Tray;
+
+namespace WinAppTests.Tray;
+
+[TestClass]
+public class TrayWorkerTests
+{
+    private TrayWorker _worker;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        _worker = new TrayWorker();
+    }
+}
