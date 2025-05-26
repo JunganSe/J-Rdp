b8a6d2aebc98418c914a954d99f1056d725bccca
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-05-26 22:27:29 +0200

    Add TrayWorker unit test: CreateNotifyIcon

--- Tests/WinAppTests/Tray/TrayWorkerTests.cs
+++ Tests/WinAppTests/Tray/TrayWorkerTests.cs
@@ -1,4 +1,6 @@
-﻿using WinApp.Tray;
+﻿#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+
+using WinApp.Tray;
 
 namespace WinAppTests.Tray;
 
@@ -11,5 +13,23 @@ public class TrayWorkerTests
     public void Setup()
     {
         _worker = new TrayWorker();
+    }
+
+    [TestMethod]
+    public void CreateNotifyIcon_ReturnsValid()
+    {
+        // Arrange
+
+        // Act
+        var trayIcon = _worker.CreateNotifyIcon();
+
+        // Assert
+        Assert.IsNotNull(trayIcon);
+        Assert.IsFalse(string.IsNullOrEmpty(trayIcon.Text));
+        Assert.IsNotNull(trayIcon.Icon);
+        Assert.IsTrue(trayIcon.Visible);
+
+        // Cleanup
+        _worker.DisposeTray(trayIcon);
     }
 }
