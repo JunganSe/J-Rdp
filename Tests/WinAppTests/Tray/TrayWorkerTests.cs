1c72cd7c610d17b85fcdb2e291d7e05552021ec6
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-06-03 21:58:07 +0200

    Move TrayWorker tests to separate class

--- Tests/WinAppTests/Tray/TrayWorkerTests.cs
+++ Tests/WinAppTests/Tray/TrayWorkerTests.cs
@@ -1,6 +1,5 @@
 ﻿#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
 #pragma warning disable IDE0039 // Use local function
-#pragma warning disable IDE0350 // Use implicitly typed lambda
 
 using Core.Profiles;
 using System.Windows.Forms;
@@ -18,74 +17,6 @@ public class TrayWorkerTests
     {
         _worker = new TrayWorker();
     }
-
-    #region Creation
-
-    [TestMethod]
-    public void CreateNotifyIcon_ReturnsValid()
-    {
-        // Arrange
-
-        // Act
-        var trayIcon = _worker.CreateNotifyIcon();
-
-        // Assert
-        Assert.IsNotNull(trayIcon);
-        Assert.IsNotNull(trayIcon.Icon);
-        Assert.IsFalse(string.IsNullOrEmpty(trayIcon.Text));
-        Assert.IsTrue(trayIcon.Visible);
-
-        // Cleanup
-        _worker.DisposeTray(trayIcon);
-    }
-
-    [TestMethod]
-    public void CreateContextMenu_NullToggleConsoleCallback_ReturnsNull()
-    {
-        // Arrange
-        Action<bool>? callback_ToggleConsole = null;
-        Action? callback_OpenConfigFile = () => { };
-
-        // Act
-        var result = _worker.CreateContextMenu(callback_ToggleConsole, callback_OpenConfigFile);
-
-        // Assert
-        Assert.IsNull(result);
-    }
-
-    [TestMethod]
-    public void CreateContextMenu_NullOpenConfigFileCallback_ReturnsNull()
-    {
-        // Arrange
-        Action<bool>? callback_ToggleConsole = (bool _) => { };
-        Action? callback_OpenConfigFile = null;
-
-        // Act
-        var result = _worker.CreateContextMenu(callback_ToggleConsole, callback_OpenConfigFile);
-
-        // Assert
-        Assert.IsNull(result);
-    }
-
-    [TestMethod]
-    public void CreateContextMenu_ValidCallbacks_ReturnsContextMenuStrip()
-    {
-        // Arrange
-        Action<bool>? callback_ToggleConsole = (bool _) => { };
-        Action? callback_OpenConfigFile = () => { };
-
-        // Act
-        var menu = _worker.CreateContextMenu(callback_ToggleConsole, callback_OpenConfigFile);
-
-        // Assert
-        Assert.IsNotNull(menu);
-        Assert.IsTrue(menu.Items.Count > 0);
-
-        // Cleanup
-        DisposeMenu(menu);
-    }
-
-    #endregion
 
     #region Menu checked state
 
