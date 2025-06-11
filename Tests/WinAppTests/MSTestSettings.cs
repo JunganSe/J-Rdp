e4e90a85115a9adfdea249bf18a27939056257a3
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-06-11 22:57:39 +0200

    Adjust WinAppTests settings

--- Tests/WinAppTests/MSTestSettings.cs
+++ Tests/WinAppTests/MSTestSettings.cs
@@ -1,3 +1,1 @@
-﻿// Disabled Parallelize because StopSignalListenerTests sometimes fails
-// when multiple StopSignalListeners are being run in parallell.
-//[assembly: Parallelize(Scope = ExecutionScope.MethodLevel)]
+﻿[assembly: Parallelize(Scope = ExecutionScope.ClassLevel)]
