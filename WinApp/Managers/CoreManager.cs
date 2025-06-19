60729dc0dccae275978af863fd59f71c63efff0f
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-06-19 23:37:47 +0200

    Use expression body methods in CoreManager

--- WinApp/Managers/CoreManager.cs
+++ WinApp/Managers/CoreManager.cs
@@ -6,50 +6,36 @@ internal class CoreManager
 {
     private Core.Controller? _controller;
 
-    public void Initialize()
-    {
+    public void Initialize() =>
         _controller = new();
-    }
 
-    public void Run()
-    {
+    public void Run() =>
         _controller?.Run();
-    }
 
-    public void Stop()
-    {
+    public void Stop() =>
         _controller?.Stop();
-    }
 
     /// <summary>
     /// Tell the core controller which method should be called after the config (in memory) has been updated.
     /// </summary>
-    public void SetCallback_ConfigUpdated(ProfileHandler callback)
-    {
+    public void SetCallback_ConfigUpdated(ProfileHandler callback) =>
         _controller?.SetCallback_ConfigUpdated(callback);
-    }
 
     /// <summary>
     /// Tell the core controller to open the folder containing the log files, if such a rule exists.
     /// </summary>
-    public void OpenLogsFolder()
-    {
+    public void OpenLogsFolder() =>
         _controller?.OpenLogsFolder();
-    }
 
     /// <summary>
     /// Tell the core controller to open the config file in the default editor.
     /// </summary>
-    public void OpenConfigFile()
-    {
+    public void OpenConfigFile() =>
         _controller?.OpenConfigFile();
-    }
 
     /// <summary>
     /// Tell the core controller to update the profiles enabled state (in config file and memory).
     /// </summary>
-    public void UpdateProfilesEnabledState(List<ProfileInfo> profileInfos)
-    {
+    public void UpdateProfilesEnabledState(List<ProfileInfo> profileInfos) =>
         _controller?.UpdateProfilesEnabledState(profileInfos);
-    }
 }
