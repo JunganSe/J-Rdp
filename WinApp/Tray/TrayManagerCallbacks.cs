417da5420116a2616cf634119400c31c85807ce2
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-06-20 23:34:51 +0200

    Add TrayManagerCallbacks class

---
+++ WinApp/Tray/TrayManagerCallbacks.cs
@@ -0,0 +1,11 @@
+﻿using Core.Profiles;
+
+namespace WinApp.Tray;
+
+internal class TrayManagerCallbacks
+{
+    public Action<bool>? ToggleConsole { get; set; }
+    public Action? OpenLogsFolder { get; set; }
+    public Action? OpenConfigFile { get; set; }
+    public ProfileHandler? ProfilesActiveStateChanged { get; set; }
+}
