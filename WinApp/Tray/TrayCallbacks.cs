29aed61591b40b46bdec6a3631bb050986551f76
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-06-22 09:21:31 +0200

    Whitespace

--- WinApp/Tray/TrayCallbacks.cs
+++ WinApp/Tray/TrayCallbacks.cs
@@ -4,7 +4,7 @@ namespace WinApp.Tray;
 
 internal class TrayCallbacks
 {
-    public Action<bool> ToggleConsole { get; set; } = (_)=> { };
+    public Action<bool> ToggleConsole { get; set; } = (_) => { };
     public Action OpenLogsFolder { get; set; } = () => { };
     public Action OpenConfigFile { get; set; } = () => { };
     public ProfileHandler ProfilesActiveStateChanged { get; set; } = (_) => { };
