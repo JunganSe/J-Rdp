dbd1074156bd21c00b5c4181c182946ac854f2c6
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-06-24 07:37:37 +0200

    Add xml comment

--- WinApp/LogConsole/ConsoleWorker.cs
+++ WinApp/LogConsole/ConsoleWorker.cs
@@ -50,6 +50,7 @@ internal partial class ConsoleWorker
     public void SetCallback_ConsoleClosed(Action callback) =>
         _callback_ConsoleClosed = callback;
 
+    /// <summary> Attempts to open a new console window. </summary>
     public bool TryAllocateConsole()
     {
         string errorMessage = "Error opening log console. Allocation failed.";
