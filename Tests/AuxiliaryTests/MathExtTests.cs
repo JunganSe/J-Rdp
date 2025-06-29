4ee4eea55cc9397ab0b590bdf2aa1f7850f50fe9
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-06-29 22:57:19 +0200

    Add MathExt.Median unit test

--- Tests/AuxiliaryTests/MathExtTests.cs
+++ Tests/AuxiliaryTests/MathExtTests.cs
@@ -29,4 +29,23 @@ public sealed class MathExtTests
 
         Assert.AreEqual(-1.5, MathExt.Median(-1.5, -2, 0));
     }
+
+    [TestMethod]
+    public void Median_HandlesExtremeValues()
+    {
+        // Min/max values for integers
+        Assert.AreEqual(0, MathExt.Median(0, int.MinValue, int.MaxValue));
+        Assert.AreEqual(int.MinValue, MathExt.Median(0, int.MinValue, int.MinValue));
+        Assert.AreEqual(int.MinValue, MathExt.Median(int.MinValue, int.MinValue, int.MaxValue));
+
+        // Min/max values for doubles
+        Assert.AreEqual(1.5, MathExt.Median(1.5, double.MinValue, double.MaxValue));
+        Assert.AreEqual(double.MinValue, MathExt.Median(double.MinValue, double.MinValue, double.MaxValue));
+
+        // Infinity
+        Assert.AreEqual(1, MathExt.Median(double.NegativeInfinity, 1.0, 2.0));
+        Assert.AreEqual(2, MathExt.Median(double.PositiveInfinity, 1.0, 2.0));
+        Assert.AreEqual(double.NegativeInfinity, MathExt.Median(double.NegativeInfinity, double.NegativeInfinity, double.NegativeInfinity));
+        Assert.AreEqual(double.PositiveInfinity, MathExt.Median(double.PositiveInfinity, 1, double.PositiveInfinity));
+    }
 }
