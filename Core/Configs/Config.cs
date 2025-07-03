22a24f93a0e986d097b5bdbcd95091ed821e849e
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-07-03 23:19:22 +0200

    Add props to config model

--- Core/Configs/Config.cs
+++ Core/Configs/Config.cs
@@ -6,5 +6,7 @@ internal class Config
 {
     public int PollingInterval { get; set; }    = ConfigConstants.PollingInterval_Default;
     public int DeleteDelay { get; set; }        = ConfigConstants.DeleteDelay_Default;
+    public bool ShowLogConsole { get; set; }    = false;
+    public bool LogToFile { get; set; }         = false;
     public List<Profile> Profiles { get; set; } = [];
 }
