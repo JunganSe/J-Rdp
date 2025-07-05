df280f44beb6cbd852643f21e21dfeac41260366
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-07-06 00:06:05 +0200

    Add ConfigInfo class

---
+++ Core/Configs/ConfigInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Configs;
+
+/// <summary>
+/// Used to represent config settings when communicating with the UI.
+/// </summary>
+public class ConfigInfo
+{
+    public bool ShowLogConsole { get; set; }
+    public bool LogToFile { get; set; }
+}
