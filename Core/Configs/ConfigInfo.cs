1c07f707ffd06cfba18defea759f5bfa73f74771
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-07-17 21:54:39 +0200

    Make ConfigInfo props nullable

--- Core/Configs/ConfigInfo.cs
+++ Core/Configs/ConfigInfo.cs
@@ -1,10 +1,11 @@
 ﻿namespace Core.Configs;
 
 /// <summary>
-/// Used to represent config settings when communicating with the UI.
+/// Used to represent config settings when communicating with the UI. <br/>
+/// Properties that are null will be ignored.
 /// </summary>
 public class ConfigInfo
 {
-    public bool ShowLogConsole { get; set; }
-    public bool LogToFile { get; set; }
+    public bool? ShowLogConsole { get; set; }
+    public bool? LogToFile { get; set; }
 }
