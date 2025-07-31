b2cfa5828c3212e0fbd6a666b0eb425397fd4d57
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-07-31 22:56:25 +0200

    Add AssemblyHelper unit tests

---
+++ Tests/AuxiliaryTests/AssemblyHelperTests.cs
@@ -0,0 +1,33 @@
+﻿using Auxiliary;
+using System.Reflection;
+
+namespace AuxiliaryTests;
+
+[TestClass]
+public sealed class AssemblyHelperTests
+{
+    [TestMethod]
+    public void GetCallingAssemblyName_ReturnsCorrectName()
+    {
+        // Arrange
+
+        // Act
+        string actual = AssemblyHelper.GetCallingAssemblyName();
+
+        // Assert
+        Assert.AreEqual("AuxiliaryTests", actual);
+    }
+
+    [TestMethod]
+    public void GetAssemblyName_ReturnsCorrectName()
+    {
+        // Arrange
+        Assembly assembly = typeof(AssemblyHelper).Assembly;
+
+        // Act
+        string actual = AssemblyHelper.GetAssemblyName(assembly);
+
+        // Assert
+        Assert.AreEqual("Auxiliary", actual);
+    }
+}
