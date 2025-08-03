e0960def906b097a91467380df01016766636dc5
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-08-03 21:48:41 +0200

    Create empty ProfileComparer

---
+++ Core/Profiles/ProfileComparer.cs
@@ -0,0 +1,14 @@
+﻿namespace Core.Profiles;
+
+internal class EqualityComparer_Profile_AllExceptId : IEqualityComparer<Profile>
+{
+    public bool Equals(Profile? a, Profile? b)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int GetHashCode(Profile obj)
+    {
+        throw new NotImplementedException();
+    }
+}
