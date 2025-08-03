4cfe850a842ba19bb5d32f0acc9462f2208243f8
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-08-03 22:10:35 +0200

    Implement ProfileComparer

--- Core/Profiles/ProfileComparer.cs
+++ Core/Profiles/ProfileComparer.cs
@@ -2,13 +2,41 @@
 
 internal class EqualityComparer_Profile_AllExceptId : IEqualityComparer<Profile>
 {
+    public record ProfileKey(
+        string Name,
+        bool Enabled,
+        string WatchFolder,
+        string Filter,
+        string MoveToFolder,
+        bool Launch,
+        bool Delete,
+        List<string> Settings);
+
+    private static ProfileKey GetKey(Profile profile) => new(
+        profile.Name,
+        profile.Enabled,
+        profile.WatchFolder,
+        profile.Filter,
+        profile.MoveToFolder,
+        profile.Launch,
+        profile.Delete,
+        profile.Settings);
+
+
     public bool Equals(Profile? a, Profile? b)
     {
-        throw new NotImplementedException();
+        if (a is null && b is null)
+            return true;
+        if (a is null || b is null)
+            return false;
+
+        var keyA = GetKey(a);
+        var keyB = GetKey(b);
+        return EqualityComparer<ProfileKey>.Default.Equals(keyA, keyB);
     }
 
     public int GetHashCode(Profile obj)
     {
-        throw new NotImplementedException();
+        return EqualityComparer<ProfileKey>.Default.GetHashCode(GetKey(obj));
     }
 }
