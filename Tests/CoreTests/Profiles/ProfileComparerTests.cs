ccb7daf90b743f7c00972863f6c9120f28edc16d
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-08-04 23:43:15 +0200

    Add profile comparer unit tests

--- Tests/CoreTests/Profiles/ProfileComparerTests.cs
+++ Tests/CoreTests/Profiles/ProfileComparerTests.cs
@@ -35,15 +35,82 @@ public class ProfileComparerTests
         Assert.IsTrue(result);
     }
 
+    [TestMethod]
+    [DataRow("Profile1", true, "C:/Foo", "*.rdp", "C:/Bar", true, false, "Setting1", "Setting2", "Setting3")]
+    [DataRow("Profile1", true, "C:/FOO", "*.rdp", "C:/BAR", true, false, "Setting1", "Setting2", "Setting3")]
+    [DataRow("Profile1", true, "C:/Foo", "*.rdp", "C:/Bar", true, false, "Setting2", "Setting1", "Setting3")]
+    public void CompareProfiles_DifferentProfiles_ReturnsTrue(
+        string name, bool enabled, string watchFolder, string filter,
+        string moveToFolder, bool launch, bool delete,
+        string setting1, string setting2, string setting3)
+    {
+        // Arrange
+        var profile1 = GetMockProfile(id: 1);
+        var profile2 = new Profile()
+        {
+            Name = name,
+            Enabled = enabled,
+            WatchFolder = watchFolder,
+            Filter = filter,
+            MoveToFolder = moveToFolder,
+            Launch = launch,
+            Delete = delete,
+            Settings = [setting1, setting2, setting3]
+        };
+
+        // Act
+        bool result = _comparer.Equals(profile1, profile2);
+
+        // Assert
+        Assert.IsTrue(result);
+    }
+
+    [TestMethod]
+    [DataRow("Profile2", true, "C:/Foo", "*.rdp", "C:/Bar", true, false, "Setting1", "Setting2", "Setting3")]
+    [DataRow("Profile1", false, "C:/Foo", "*.rdp", "C:/Bar", true, false, "Setting1", "Setting2", "Setting3")]
+    [DataRow("Profile1", true, "C:/AAA", "*.rdp", "C:/Bar", true, false, "Setting1", "Setting2", "Setting3")]
+    [DataRow("Profile1", true, "C:/Foo", "*.AAA", "C:/Bar", true, false, "Setting1", "Setting2", "Setting3")]
+    [DataRow("Profile1", true, "C:/Foo", "*.rdp", "C:/AAA", true, false, "Setting1", "Setting2", "Setting3")]
+    [DataRow("Profile1", true, "C:/Foo", "*.rdp", "C:/Bar", false, false, "Setting1", "Setting2", "Setting3")]
+    [DataRow("Profile1", true, "C:/Foo", "*.rdp", "C:/Bar", true, true, "Setting1", "Setting2", "Setting3")]
+    [DataRow("Profile1", true, "C:/Foo", "*.rdp", "C:/Bar", true, false, "SettingA", "Setting2", "Setting3")]
+    [DataRow("Profile1", true, "C:/Foo", "*.rdp", "C:/Bar", true, false, "Setting1", "SettingB", "Setting3")]
+    [DataRow("Profile1", true, "C:/Foo", "*.rdp", "C:/Bar", true, false, "Setting1", "Setting2", "SettingC")]
+    public void CompareProfiles_DifferentProfiles_ReturnsFalse(
+        string name, bool enabled, string watchFolder, string filter,
+        string moveToFolder, bool launch, bool delete,
+        string setting1, string setting2, string setting3)
+    {
+        // Arrange
+        var profile1 = GetMockProfile(id: 1);
+        var profile2 = new Profile()
+        {
+            Name = name,
+            Enabled = enabled,
+            WatchFolder = watchFolder,
+            Filter = filter,
+            MoveToFolder = moveToFolder,
+            Launch = launch,
+            Delete = delete,
+            Settings = [setting1, setting2, setting3]
+        };
+
+        // Act
+        bool result = _comparer.Equals(profile1, profile2);
+
+        // Assert
+        Assert.IsFalse(result);
+    }
+
     #region Mocks
 
     private Profile GetMockProfile(int id) => new(id)
     {
         Name = "Profile1",
         Enabled = true,
-        WatchFolder = "C:\\Foo",
+        WatchFolder = "C:/Foo",
         Filter = "*.rdp",
-        MoveToFolder = "C:\\Bar",
+        MoveToFolder = "C:/Bar",
         Launch = true,
         Delete = false,
         Settings = ["Setting1", "Setting2", "Setting3"]
