f208624b2e57df49d02a771c13c9333b37fbe4b0
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-08-04 23:26:59 +0200

    Refactor unit tests

--- Tests/CoreTests/Profiles/ProfileComparerTests.cs
+++ Tests/CoreTests/Profiles/ProfileComparerTests.cs
@@ -11,30 +11,8 @@ public class ProfileComparerTests
     public void CompareProfiles_EqualProfilesWithSameId_ReturnsTrue()
     {
         // Arrange
-        var profile1 = new Profile()
-        {
-            Id = 1,
-            Name = "Profile1",
-            Enabled = true,
-            WatchFolder = "C:\\Foo",
-            Filter = "*.rdp",
-            MoveToFolder = "C:\\Bar",
-            Launch = true,
-            Delete = false,
-            Settings = ["Setting1", "Setting2"]
-        };
-        var profile2 = new Profile()
-        {
-            Id = 1,
-            Name = "Profile1",
-            Enabled = true,
-            WatchFolder = "C:\\Foo",
-            Filter = "*.rdp",
-            MoveToFolder = "C:\\Bar",
-            Launch = true,
-            Delete = false,
-            Settings = ["Setting1", "Setting2"]
-        };
+        var profile1 = GetMockProfile(id: 1);
+        var profile2 = GetMockProfile(id: 1);
 
         // Act
         bool result = _comparer.Equals(profile1, profile2);
@@ -47,30 +25,8 @@ public class ProfileComparerTests
     public void CompareProfiles_EqualProfilesWithDifferentId_ReturnsTrue()
     {
         // Arrange
-        var profile1 = new Profile()
-        {
-            Id = 1,
-            Name = "Profile1",
-            Enabled = true,
-            WatchFolder = "C:\\Foo",
-            Filter = "*.rdp",
-            MoveToFolder = "C:\\Bar",
-            Launch = true,
-            Delete = false,
-            Settings = ["Setting1", "Setting2"]
-        };
-        var profile2 = new Profile()
-        {
-            Id = 2,
-            Name = "Profile1",
-            Enabled = true,
-            WatchFolder = "C:\\Foo",
-            Filter = "*.rdp",
-            MoveToFolder = "C:\\Bar",
-            Launch = true,
-            Delete = false,
-            Settings = ["Setting1", "Setting2"]
-        };
+        var profile1 = GetMockProfile(id: 1);
+        var profile2 = GetMockProfile(id: 2);
 
         // Act
         bool result = _comparer.Equals(profile1, profile2);
@@ -78,4 +34,20 @@ public class ProfileComparerTests
         // Assert
         Assert.IsTrue(result);
     }
+
+    #region Mocks
+
+    private Profile GetMockProfile(int id) => new(id)
+    {
+        Name = "Profile1",
+        Enabled = true,
+        WatchFolder = "C:\\Foo",
+        Filter = "*.rdp",
+        MoveToFolder = "C:\\Bar",
+        Launch = true,
+        Delete = false,
+        Settings = ["Setting1", "Setting2, Setting3"]
+    };
+
+    #endregion
 }
