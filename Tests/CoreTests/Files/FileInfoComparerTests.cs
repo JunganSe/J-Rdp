242cb9d6e5e27b89b890dae4882220f3c665011a
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-08-05 22:29:14 +0200

    Add FileInfo comparer unit tests

---
+++ Tests/CoreTests/Files/FileInfoComparerTests.cs
@@ -0,0 +1,37 @@
+﻿using Core.Files;
+
+namespace CoreTests.Files;
+
+[TestClass]
+public class FileInfoComparerTests
+{
+    private readonly EqualityComparer_FileInfo_FullName _comparer = new();
+
+    [TestMethod]
+    public void SamePath_ReturnsTrue()
+    {
+        // Arrange
+        var fileInfo1 = new FileInfo("C:/Foo");
+        var fileInfo2 = new FileInfo("C:/Foo");
+
+        // Act
+        bool result = _comparer.Equals(fileInfo1, fileInfo2);
+
+        // Assert
+        Assert.IsTrue(result);
+    }
+
+    [TestMethod]
+    public void DifferentPath_ReturnsFalse()
+    {
+        // Arrange
+        var fileInfo1 = new FileInfo("C:/Foo");
+        var fileInfo2 = new FileInfo("C:/Bar");
+
+        // Act
+        bool result = _comparer.Equals(fileInfo1, fileInfo2);
+
+        // Assert
+        Assert.IsFalse(result);
+    }
+}
