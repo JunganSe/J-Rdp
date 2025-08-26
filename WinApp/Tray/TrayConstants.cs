d18a2023dccb6b32395bba7007d97cd960e4112f
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-08-26 21:09:41 +0200

    Reorder constants

--- WinApp/Tray/TrayConstants.cs
+++ WinApp/Tray/TrayConstants.cs
@@ -4,26 +4,26 @@ internal static class TrayConstants
 {
     public static class ItemNames
     {
-        public const string LoggingSubMenu = "LoggingSubMenu";
-        public const string ToggleConsole = "ShowConsole";
-        public const string ToggleFileLogging = "LogToFile";
-        public const string OpenLogsFolder = "OpenLogsFolder";
         public const string OpenConfigFile = "OpenConfigFile";
         public const string ProfilesInsertPoint = "ProfilesInsertPoint";
         public const string ProfilePrefix = "Profile";
         public const string PlaceholderProfile = "PlaceholderProfile";
+        public const string LoggingSubMenu = "LoggingSubMenu";
+        public const string OpenLogsFolder = "OpenLogsFolder";
+        public const string ToggleConsole = "ShowConsole";
+        public const string ToggleFileLogging = "LogToFile";
         public const string Close = "Close";
         public const string Exit = "Exit";
     }
 
     public static class ItemTexts
     {
-        public const string LoggingSubMenu = "Logging";
-        public const string ToggleConsole = "Show log console";
-        public const string ToggleFileLogging = "Log to file";
-        public const string OpenLogsFolder = "Open logs folder";
         public const string OpenConfigFile = "Open config file";
         public const string PlaceholderProfile = "No profiles found";
+        public const string LoggingSubMenu = "Logging";
+        public const string OpenLogsFolder = "Open logs folder";
+        public const string ToggleConsole = "Show log console";
+        public const string ToggleFileLogging = "Log to file";
         public const string Close = "Close menu";
         public const string Exit = "Exit";
     }
