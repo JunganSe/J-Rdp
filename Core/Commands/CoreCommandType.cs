7b779e3daa6f94f55e0d37e13e266315a81af425
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-11-04 19:40:16 +0100

    Add enum CoreCommandType

---
+++ Core/Commands/CoreCommandType.cs
@@ -0,0 +1,14 @@
+﻿namespace Core.Commands;
+
+public enum CoreCommandType
+{
+    Initialize,
+    Run,
+    Stop,
+    SetLogConsoleVisibility,
+    SetLogToFile,
+    OpenLogsFolder,
+    OpenConfigFile,
+    UpdateConfig,
+    SetCallback_ConfigUpdated,
+}
