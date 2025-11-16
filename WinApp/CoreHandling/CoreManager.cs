c095c7994b26c8458bc023b2cc562339f0531fd6
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-11-16 18:16:58 +0100

    Update xml comments

--- WinApp/CoreHandling/CoreManager.cs
+++ WinApp/CoreHandling/CoreManager.cs
@@ -9,7 +9,7 @@ internal class CoreManager
     private Core.Controller? _coreController;
 
     /// <summary>
-    /// Initialize the core controller.
+    /// Create and initialize the core controller.
     /// </summary>
     public void Initialize(ControllerInitParams coreControllerInitParams) =>
         _coreController = new(coreControllerInitParams);
@@ -20,45 +20,30 @@ internal class CoreManager
     public void Stop() =>
         _coreController?.Stop();
 
-    /// <summary>
-    /// Tell the core controller to open the folder containing the log files, if such a rule exists.
-    /// </summary>
     public void OpenLogsFolder()
     {
         var command = new CoreCommand(CoreCommandType.OpenLogsFolder);
         _coreController?.ExecuteCommand(command);
     }
 
-    /// <summary>
-    /// Tell the core controller to show the log display.
-    /// </summary>
     public void ShowLog(bool showLog)
     {
         var command = new CoreCommand(CoreCommandType.ShowLog, showLog);
         _coreController?.ExecuteCommand(command);
     }
 
-    /// <summary>
-    /// Tell the core controller to enable or disable logging to file.
-    /// </summary>
     public void SetLogToFile(bool logToFile)
     {
         var command = new CoreCommand(CoreCommandType.SetLogToFile, logToFile);
         _coreController?.ExecuteCommand(command);
     }
 
-    /// <summary>
-    /// Tell the core controller to open the config file in the default editor.
-    /// </summary>
     public void OpenConfigFile()
     {
         var command = new CoreCommand(CoreCommandType.OpenConfigFile);
         _coreController?.ExecuteCommand(command);
     }
 
-    /// <summary>
-    /// Tell the core controller to update the config (in file and memory).
-    /// </summary>
     public void UpdateConfig(ConfigInfo configInfo)
     {
         var command = new CoreCommand(CoreCommandType.UpdateConfig, configInfo);
