c1c2f61003775c1a21062be9f380b5777951f9bb
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-11-22 16:23:25 +0100

    Add partially implemented ConfigChangesHelper

---
+++ Core/Configs/ConfigChangesHelper.cs
@@ -0,0 +1,28 @@
+﻿namespace Core.Configs;
+
+internal static class ConfigChangesHelper
+{
+    public static List<string> GetChangedSettings(Config oldConfig, Config newConfig)
+    {
+        var changedSettings = new List<string>();
+
+        if (newConfig.PollingInterval != oldConfig.PollingInterval)
+            changedSettings.Add(GetChangeSummary(nameof(Config.PollingInterval), oldConfig.PollingInterval, newConfig.PollingInterval));
+
+        if (newConfig.DeleteDelay != oldConfig.DeleteDelay)
+            changedSettings.Add(GetChangeSummary(nameof(Config.DeleteDelay), oldConfig.DeleteDelay, newConfig.DeleteDelay));
+
+        if (newConfig.ShowLog != oldConfig.ShowLog)
+            changedSettings.Add(GetChangeSummary(nameof(Config.ShowLog), oldConfig.ShowLog, newConfig.ShowLog));
+
+        if (newConfig.LogToFile != oldConfig.LogToFile)
+            changedSettings.Add(GetChangeSummary(nameof(Config.LogToFile), oldConfig.LogToFile, newConfig.LogToFile));
+
+        // TODO: Compare Profiles.
+
+        return changedSettings;
+    }
+
+    private static string GetChangeSummary<T>(string propertyName, T oldValue, T newValue) =>
+        $"{propertyName}: {oldValue} => {newValue}";
+}
