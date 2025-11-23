f264c5fa33f74ee981d845cfd739d695bf347afd
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-11-23 21:45:51 +0100

    Refactor ConfigChangesHelper

--- Core/Configs/ConfigChangesHelper.cs
+++ Core/Configs/ConfigChangesHelper.cs
@@ -1,10 +1,19 @@
-﻿using System.Runtime.CompilerServices;
+﻿using Core.Profiles;
+using System.Runtime.CompilerServices;
 
 namespace Core.Configs;
 
 internal static class ConfigChangesHelper
 {
     public static List<string> GetChangedSettings(Config oldConfig, Config newConfig)
+    {
+        var changedConfigSettings = GetChangedConfigSettings(oldConfig, newConfig);
+        var changedProfileSettings = GetChangedProfilesSettings(oldConfig, newConfig);
+
+        return [.. changedConfigSettings, .. changedProfileSettings];
+    }
+
+    public static List<string> GetChangedConfigSettings(Config oldConfig, Config newConfig)
     {
         var changedSettings = new List<string>();
 
@@ -20,15 +29,19 @@ internal static class ConfigChangesHelper
         if (newConfig.LogToFile != oldConfig.LogToFile)
             changedSettings.Add(GetChangeSummary(oldConfig.LogToFile, newConfig.LogToFile));
 
-        // TODO: Compare Profiles.
-
         return changedSettings;
     }
 
     private static string GetChangeSummary<T>(T oldValue, T newValue,
-        [CallerArgumentExpression(nameof(newValue))] string? newValueExpression = null) // Generates a string from the argument. E.g. "newConfig.PollingInterval"
+        [CallerArgumentExpression(nameof(newValue))] string? newValueExpression = null) // Generates a string from the argument used when calling, e.g. "newConfig.PollingInterval"
     {
         string propertyName = newValueExpression?.Split('.').LastOrDefault() ?? "(Unknown)";
         return $"{propertyName}: {oldValue} => {newValue}";
+    }
+
+    public static List<string> GetChangedProfilesSettings(Config oldConfig, Config newConfig)
+    {
+        // TODO: Implement profile comparison logic.
+        throw new NotImplementedException();
     }
 }
