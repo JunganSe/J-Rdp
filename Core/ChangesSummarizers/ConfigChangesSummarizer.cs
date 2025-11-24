bc68d56bda032295a622e386655ebf2eec2b92c2
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-11-24 22:12:28 +0100

    Move GetChangedProfilesSettings to new class

--- Core/ChangesSummarizers/ConfigChangesSummarizer.cs
+++ Core/ChangesSummarizers/ConfigChangesSummarizer.cs
@@ -30,35 +30,4 @@ internal static class ConfigChangesSummarizer
         string propertyName = newValueExpression?.Split('.').LastOrDefault() ?? "(Unknown)";
         return $"{propertyName}: {oldValue} => {newValue}";
     }
-
-    // TODO: Move to separate class "ProfileChangesSummarizer" and use profiles collections as parameters.
-    public static List<string> GetChangedProfilesSettings(Config oldConfig, Config newConfig)
-    {
-        var keptProfiles = newConfig.Profiles
-            .Where(np => oldConfig.Profiles.Any(op => op.Id == np.Id))
-            .ToList();
-        var removedProfiles = oldConfig.Profiles
-            .Where(op => newConfig.Profiles.All(np => np.Id != op.Id))
-            .ToList();
-        var newProfiles = newConfig.Profiles
-            .Where(np => oldConfig.Profiles.All(op => op.Id != np.Id))
-            .ToList();
-
-        var output = new List<string>();
-
-        if (newProfiles.Count > 0)
-            output.AddRange(newProfiles.Select(np => $"Profile added: {np.Name}"));
-
-        if (removedProfiles.Count > 0)
-            output.AddRange(removedProfiles.Select(rp => $"Profile removed: {rp.Name}"));
-
-        foreach (var keptProfile in keptProfiles)
-        {
-            var oldProfile = oldConfig.Profiles.First(op => op.Id == keptProfile.Id);
-            var profileChanges = ProfileChangesSummarizer.GetChangesSummary(oldProfile, keptProfile);
-            output.AddRange(profileChanges.Select(change => $"Profile '{keptProfile.Name}': {change}"));
-        }
-
-        return output;
-    }
 }
---
+++ Core/ChangesSummarizers/ProfileChangesSummarizer.cs
@@ -0,0 +1,36 @@
+﻿using Core.Configs;
+
+namespace Core.ChangesSummarizers;
+
+internal static class ProfileChangesSummarizer
+{
+    public static List<string> GetChangedProfilesSettings(Config oldConfig, Config newConfig)
+    {
+        var keptProfiles = newConfig.Profiles
+            .Where(np => oldConfig.Profiles.Any(op => op.Id == np.Id))
+            .ToList();
+        var removedProfiles = oldConfig.Profiles
+            .Where(op => newConfig.Profiles.All(np => np.Id != op.Id))
+            .ToList();
+        var newProfiles = newConfig.Profiles
+            .Where(np => oldConfig.Profiles.All(op => op.Id != np.Id))
+            .ToList();
+
+        var output = new List<string>();
+
+        if (newProfiles.Count > 0)
+            output.AddRange(newProfiles.Select(np => $"Profile added: {np.Name}"));
+
+        if (removedProfiles.Count > 0)
+            output.AddRange(removedProfiles.Select(rp => $"Profile removed: {rp.Name}"));
+
+        foreach (var keptProfile in keptProfiles)
+        {
+            var oldProfile = oldConfig.Profiles.First(op => op.Id == keptProfile.Id);
+            var profileChanges = GetChangesSummary(oldProfile, keptProfile);
+            output.AddRange(profileChanges.Select(change => $"Profile '{keptProfile.Name}': {change}"));
+        }
+
+        return output;
+    }
+}
