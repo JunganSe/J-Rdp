f056174c771449ac383daec4e1c9ee2e387386da
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-11-24 22:31:43 +0100

    Update ProfileChangesSummarizer

--- Core/ChangesSummarizers/ProfileChangesSummarizer.cs
+++ Core/ChangesSummarizers/ProfileChangesSummarizer.cs
@@ -1,32 +1,32 @@
-﻿using Core.Configs;
+﻿using Core.Profiles;
 
 namespace Core.ChangesSummarizers;
 
 internal static class ProfileChangesSummarizer
 {
-    public static List<string> GetChangedProfilesSettings(Config oldConfig, Config newConfig)
+    public static List<string> GetChangedProfilesSettings(List<Profile> oldProfiles, List<Profile> newProfiles)
     {
-        var keptProfiles = newConfig.Profiles
-            .Where(np => oldConfig.Profiles.Any(op => op.Id == np.Id))
+        var keptProfiles = newProfiles
+            .Where(np => oldProfiles.Any(op => op.Id == np.Id))
             .ToList();
-        var removedProfiles = oldConfig.Profiles
-            .Where(op => newConfig.Profiles.All(np => np.Id != op.Id))
+        var removedProfiles = oldProfiles
+            .Where(op => newProfiles.All(np => np.Id != op.Id))
             .ToList();
-        var newProfiles = newConfig.Profiles
-            .Where(np => oldConfig.Profiles.All(op => op.Id != np.Id))
+        var addedProfiles = newProfiles
+            .Where(np => oldProfiles.All(op => op.Id != np.Id))
             .ToList();
 
         var output = new List<string>();
 
-        if (newProfiles.Count > 0)
-            output.AddRange(newProfiles.Select(np => $"Profile added: {np.Name}"));
+        if (addedProfiles.Count > 0)
+            output.AddRange(addedProfiles.Select(np => $"Profile added: {np.Name}"));
 
         if (removedProfiles.Count > 0)
             output.AddRange(removedProfiles.Select(rp => $"Profile removed: {rp.Name}"));
 
         foreach (var keptProfile in keptProfiles)
         {
-            var oldProfile = oldConfig.Profiles.First(op => op.Id == keptProfile.Id);
+            var oldProfile = oldProfiles.First(op => op.Id == keptProfile.Id);
             var profileChanges = GetChangesSummary(oldProfile, keptProfile);
             output.AddRange(profileChanges.Select(change => $"Profile '{keptProfile.Name}': {change}"));
         }
