3720aefd3a5f696eec0905fefe5d98a2f86d6496
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-11-25 21:56:28 +0100

    Move GetSettingsChanges to new class

--- Core/ChangesSummarizers/ProfileChangesSummarizer.cs
+++ Core/ChangesSummarizers/ProfileChangesSummarizer.cs
@@ -60,7 +60,7 @@ internal static class ProfileChangesSummarizer
         if (newProfile.Delete != oldProfile.Delete)
             changes.Add(GetChangeSummary(oldProfile.Delete, newProfile.Delete));
 
-        var settingsChanges = GetSettingsChanges(oldProfile.Settings, newProfile.Settings);
+        var settingsChanges = ProfileSettingsChangesSummarizer.GetSettingsChanges(oldProfile.Settings, newProfile.Settings);
         changes.AddRange(settingsChanges);
 
         return changes;
@@ -71,49 +71,5 @@ internal static class ProfileChangesSummarizer
     {
         string propertyName = newValueExpression?.Split('.').LastOrDefault() ?? "(Unknown)";
         return $"{propertyName}: {oldValue} => {newValue}";
-    }
-
-    public static List<string> GetSettingsChanges(List<string> oldSettings, List<string> newSettings)
-    {
-        bool areSettingsEqual = newSettings.Order().SequenceEqual(oldSettings.Order());
-        if (areSettingsEqual)
-            return [];
-
-        var oldSettingsLookup = oldSettings.ToDictionary(s => s.Split(':')[0]);
-        var newSettingsLookup = newSettings.ToDictionary(s => s.Split(':')[0]);
-
-        var addedSettings = newSettingsLookup.Keys
-            .Where(key => !oldSettingsLookup.ContainsKey(key))
-            .Select(key => newSettingsLookup[key])
-            .ToList();
-        var removedSettings = oldSettingsLookup.Keys
-            .Where(key => !newSettingsLookup.ContainsKey(key))
-            .Select(key => oldSettingsLookup[key])
-            .ToList();
-        var changedSettings = newSettingsLookup.Keys
-            .Where(key => oldSettingsLookup.ContainsKey(key) && oldSettingsLookup[key] != newSettingsLookup[key])
-            .Select(key => (oldSetting: oldSettingsLookup[key], newSetting: newSettingsLookup[key]))
-            .ToList();
-
-        var output = new List<string>();
-
-        if (addedSettings.Count > 0)
-        {
-            string settingsWord = (addedSettings.Count == 1) ? "setting" : "settings";
-            string settings = string.Join(", ", addedSettings.Select(s => $"'{s}'"));
-            output.Add($"Added {settingsWord}: {settings}");
-        }
-
-        if (removedSettings.Count > 0)
-        {
-            string settingsWord = (removedSettings.Count == 1) ? "setting" : "settings";
-            string settings = string.Join(", ", removedSettings.Select(s => $"'{s}'"));
-            output.Add($"Removed {settingsWord}: {settings}");
-        }
-
-        foreach (var (oldSetting, newSetting) in changedSettings)
-            output.Add($"Changed setting: '{oldSetting}' => '{newSetting}'");
-
-        return output;
     }
 }
