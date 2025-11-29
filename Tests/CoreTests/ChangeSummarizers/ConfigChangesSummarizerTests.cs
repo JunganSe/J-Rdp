6ed12b96b958a50d95844dc0ce2175643fb60885
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-11-29 22:50:24 +0100

    Add ConfigChangesSummarizer unit test

---
+++ Tests/CoreTests/ChangeSummarizers/ConfigChangesSummarizerTests.cs
@@ -0,0 +1,39 @@
+﻿using Core.ChangesSummarizers;
+using Core.Configs;
+
+namespace CoreTests.ChangeSummarizers;
+
+[TestClass]
+public class ConfigChangesSummarizerTests
+{
+    [TestMethod]
+    [DataRow(0, 0, false, false)]
+    [DataRow(1, 2, true, true)]
+    [DataRow(3, 4, true, false)]
+    [DataRow(5, 5, false, true)]
+    public void GetChangedConfigSettings_NoValuesChanged_ReturnsEmptyList(
+        int pollingInterval, int deleteDelay, bool showLog, bool logToFile)
+    {
+        // Arrange
+        var oldConfig = new Config()
+        {
+            PollingInterval = pollingInterval,
+            DeleteDelay = deleteDelay,
+            ShowLog = showLog,
+            LogToFile = logToFile,
+        };
+        var newConfig = new Config()
+        {
+            PollingInterval = pollingInterval,
+            DeleteDelay = deleteDelay,
+            ShowLog = showLog,
+            LogToFile = logToFile,
+        };
+
+        // Act
+        var changes = ConfigChangesSummarizer.GetChangedConfigSettings(oldConfig, newConfig);
+
+        // Assert
+        Assert.AreEqual(0, changes.Count);
+    }
+}
