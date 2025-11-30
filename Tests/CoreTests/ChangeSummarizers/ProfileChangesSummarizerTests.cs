62ffc6e48a7c9114a88f0f607149d910267fe735
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2025-11-30 20:50:16 +0100

    Add ProfileChangesSummarizer unit test

---
+++ Tests/CoreTests/ChangeSummarizers/ProfileChangesSummarizerTests.cs
@@ -0,0 +1,58 @@
+﻿using Core.ChangesSummarizers;
+using Core.Profiles;
+
+namespace CoreTests.ChangeSummarizers;
+
+[TestClass]
+public class ProfileChangesSummarizerTests
+{
+    [TestMethod]
+    [DataRow(1, "Profile 1", true, "C:/Watch", "*.txt", "C:\\Processed", true, true)]
+    [DataRow(0, "", false, "", "", "", false, false)]
+    public void GetChangedProfilesSettings_IdenticalProfiles_ReturnsEmptyList(
+        int id, string name, bool enabled, string watchFolder,
+        string filter, string moveToFolder, bool launch, bool delete)
+    {
+        // Arrange
+        var oldProfiles = new List<Profile>()
+        {
+            new()
+            {
+                Id = id,
+                Name = name,
+                Enabled = enabled,
+                WatchFolder = watchFolder,
+                Filter = filter,
+                MoveToFolder = moveToFolder,
+                Launch = launch,
+                Delete = delete,
+                Settings = [],
+            },
+            new() { Id = 2, Name = "Profile 2" },
+            new(),
+        };
+        var newProfiles = new List<Profile>()
+        {
+            new()
+            {
+                Id = id,
+                Name = name,
+                Enabled = enabled,
+                WatchFolder = watchFolder,
+                Filter = filter,
+                MoveToFolder = moveToFolder,
+                Launch = launch,
+                Delete = delete,
+                Settings = [],
+            },
+            new() { Id = 2, Name = "Profile 2" },
+            new(),
+        };
+
+        // Act
+        List<string> changes = ProfileChangesSummarizer.GetChangedProfilesSettings(oldProfiles, newProfiles);
+
+        // Assert
+        Assert.AreEqual(0, changes.Count);
+    }
+}
